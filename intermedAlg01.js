69e1ca98c623428c5004cc6da7ab46a4ab50b562
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-02 10:36:15 -0400

    Update intermedAlg01.js

--- intermedAlg01.js
+++ intermedAlg01.js
@@ -1,3 +1,7 @@
+//This function find the sum all all numbers within a range
+//first it finds what operation will return a positive number for range
+//then the for loop will iterate as long as its still less than the range between the lowest and highest value, sum starts off being assigned the min value + i(0) then again until i reaches the range
+
 function sumAll(arr) {
     let sum = 0;
     if (arr[0]-arr[1] < 0 ) {
