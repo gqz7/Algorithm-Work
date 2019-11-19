7cb80beb95f50a93b7d8de7ddd3a01ac755e893b
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-18 23:59:34 -0500

    Add files via upload

--- palindromeCLI.js
+++ palindromeCLI.js
@@ -10,20 +10,23 @@ function isitpalindrome() {
     rl.question('Enter a string and we will return if it is a palindrome or not\nInput: ', (string) => {
       string = string.replace(/ |\_|\.|\,|\\|\/|\-|\(|\)|\:/g, '').toLowerCase();  
 
-     let reversedString = string.split('').reverse().join();
+     let reversedString = string.split('').reverse().join('');
+
+     console.log(reversedString, string);
+     
 
       if (string === reversedString) {
-          console.log('Your string is a palindrome');
+          console.log('\nYour string is a palindrome\n');
           
       } else {
-          console.log('Your string is not a palindrome');
+          console.log('\nYour string is not a palindrome\n');
           
       }
 
       rl.question('Would you like to try another word?(y/n)\nInput: ', (yon) => {
           yon = yon.trim().toLowerCase().substring(0,1);
           if (yon == 'y') {
-              console.log('Great! you will enter another word');
+              console.log('Great! you will enter another word\n____________________________________________\n');
               isitpalindrome();
           } else {
               console.log('Thank you for using the program');
