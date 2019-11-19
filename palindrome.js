522d4ea5bb360d9d5e440a086ad919ddd97b1694
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-18 23:44:10 -0500

    Add files via upload

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,16 @@
 function palindrome(str) {
-  // Good luck!
-  str = str.replace(/ |\_|\.|\,/g, '').toLowerCase();
+  // replaces special characters since they are not counted in palindromes and convert to lowercase(toUpperCase would also work) 
+  //because this alg is not supposed to be case sensitive
+  str = str.replace(/ |\_|\.|\,|\\|\/|\-|\(|\)|\:/g, '').toLowerCase();  
   let strRev = str.split('').reverse().join('');
-  console.log(strRev, str);
   
   if(strRev == str){
+      console.log('Your string is a palindrome');
+      
     return true;
   } else {
+      console.log('Your string is not a palindrome');
+      
   return false;
   }
 }
