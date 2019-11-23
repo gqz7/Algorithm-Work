84a7e12c67121f9b75acd783cf0cb8221004fa01
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-22 23:30:36 -0500

    Add files via upload

--- cashregister.js
+++ cashregister.js
@@ -1,35 +1,53 @@
 function checkCashRegister(price, cash, cid) {
-  var changeTotal = cash - price, cidTotal = 0;
+  let change = [["PENNY", 0], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]],
+      changeValues = [0.01,.05,.1,.25,1,5,10,20,100], changeRequired = (cash - price), cidTotal = 0;
 
-    for (let i = 0; i < cid.length; i++) {
-        console.log(cid[i][1], "--------------", cidTotal);
-        
+      for (let i = cid.length-1; i >= 0; i--) {
+
+        while (cid[i][1] - changeValues[i] >= 0 && changeRequired - changeValues[i] >= 0) {
+            cid[i][1] -= changeValues[i];
+            cid[i][1] = Math.round(cid[i][1] * 100) / 100;
+
+            change[i][1] += changeValues[i];
+            change[i][1] = Math.round(change[i][1] * 100) / 100;
+
+            changeRequired -= changeValues[i];
+            changeRequired = Math.round(changeRequired * 100) / 100;
+        }
         cidTotal += cid[i][1];
-        cidTotal = (cidTotal * 1000) % 1000;
-    }
-    // if (c) {
+        cidTotal = Math.round(cidTotal * 100) / 100;
+
+        // console.log(`Unit:${cid[i][0]}   Drawer:${cid[i][1]}   Change ${change[i][1]}, "Change needed:", ${changeRequired}`);
+      }
+        // console.log(change,cidTotal);
+
+      let results;
+        if (cidTotal > 0 && changeRequired == 0) {
+            
+            for (let i = 0; i < change.length; i++) {
+                if (change[i][1] == 0) {
+                    change.splice(i,1);
+                    i--
+                }
+                
+            }
+            change = change.reverse();
+            results = {status: "OPEN", change: change};
+
+        } else if (cidTotal == 0 && changeRequired == 0) {
+            results = {status: "CLOSED", change: change};
+
+        } else if ( changeRequired > 0) {
+            results = {status: "INSUFFICIENT_FUNDS", change: []};
+
+        }
+            console.log(results);
+            return results;
         
-    // }
-    
-  // Here is your change, ma'am.
-//   console.log(change);
-  
-  
-//   return change;
 }
 
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
 
-// checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
-// should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
-
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+// checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+// checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+// checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
