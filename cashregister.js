e10d7e4d61459f2a12018282dbc7471069507b29
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-22 21:24:03 -0500

    Add files via upload

---
+++ cashregister.js
@@ -0,0 +1,35 @@
+function checkCashRegister(price, cash, cid) {
+  var changeTotal = cash - price, cidTotal = 0;
+
+    for (let i = 0; i < cid.length; i++) {
+        console.log(cid[i][1], "--------------", cidTotal);
+        
+        cidTotal += cid[i][1];
+        cidTotal = (cidTotal * 1000) % 1000;
+    }
+    // if (c) {
+        
+    // }
+    
+  // Here is your change, ma'am.
+//   console.log(change);
+  
+  
+//   return change;
+}
+
+// Example cash-in-drawer array:
+// [["PENNY", 1.01],
+// ["NICKEL", 2.05],
+// ["DIME", 3.1],
+// ["QUARTER", 4.25],
+// ["ONE", 90],
+// ["FIVE", 55],
+// ["TEN", 20],
+// ["TWENTY", 60],
+// ["ONE HUNDRED", 100]]
+
+// checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
+// should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
