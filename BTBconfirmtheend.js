a1d1c779498aae0fd90ea490110a39300a831208
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-24 03:52:28 -0500

    Add files via upload

--- BTBconfirmtheend.js
+++ BTBconfirmtheend.js
@@ -11,10 +11,15 @@ function confirm() {
 
         rl.question('Enter any portion of your string including the end, and we will confirm it for you.\nEnd: ', (end) => {
 
-
-
-
-
+            // console.log(end, string.substring(string.length - end.length, string.length));
+            
+            if (end == string.substring(string.length - end.length, string.length)) {
+                console.log('\nYour string ending is confirmed true!\n');
+                
+            } else {
+                console.log('\nThe string end was false...\n');
+                
+            }
 
             rl.question('Do you want to try again?\(\'no\' to exit\): ', (yon) => {
                 if (yon == 'no') {
@@ -22,7 +27,7 @@ function confirm() {
                     rl.close()
                 } else {
                     console.log('The program will run once more...\n\n');
-                    convertTemp();
+                    confirm();
                 }
             });
         })
