50f2d4199199ee03b60e3f994b2718fe5b03ef83
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-24 04:10:12 -0500

    Add files via upload

--- BTBreverseastring.js
+++ BTBreverseastring.js
@@ -6,22 +6,27 @@ var readline = require('readline'),
 
 function reverse() {
     rl.question(
-        'Enter a string and it will be reversed and the output will be displayed for you.\nString: ',
-        (string) => {
+        'Enter a string and it will be reversed and the output will be displayed for you.\nString: ', (string) => {
+
             let reversedS = '';
+
             for (let i = string.length -1; i > -1; i--) {
                reversedS += string[i];
                 
             }
-            console.log(`\n________________________________\nYour reversed string is ${reversedS}\n________________________________\n`);
 
-            rl.question('Do you want to try again?\(\'no\' to exit\)', (yon) => {
+            console.log(`\n________________________________\nYour reversed string is: ${reversedS}\n________________________________\n`);
+
+            rl.question('Do you want to try again?\(\'no\' to exit\):  ', (yon) => {
+
                 if (yon == 'no') {
                     console.log('\nThe program will not end...');
                     rl.close()
+
                 } else {
                     console.log('The program will run once more...\n\n');
                     reverse();
+
                 }
             });
         }
