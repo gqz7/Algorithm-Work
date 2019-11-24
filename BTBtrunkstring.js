2539fa64ee96db5119bf9d342772a3fc04a61e36
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-24 04:09:11 -0500

    Add files via upload

--- BTBtrunkstring.js
+++ BTBtrunkstring.js
@@ -15,17 +15,18 @@ function trunk() {
             console.log('Your trunkated string is', trunkString);
 
             rl.question('Do you want to try again?\(\'no\' to exit\): ', (yon) => {
+
                 if (yon == 'no') {
                     console.log('\nThe program will not end...');
                     rl.close()
+
                 } else {
                     console.log('The program will run once more...\n\n');
                     repeat();
+
                 }
             });
         })
     }))
-
-
 }
 trunk()
