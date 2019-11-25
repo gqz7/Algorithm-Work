357a5ed9bf3024c53e71b195b9b3d562568b6df5
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-25 18:09:24 -0500

    Add files via upload

--- BTBlargestnum_inarrays.js
+++ BTBlargestnum_inarrays.js
@@ -17,15 +17,20 @@ function add_array() {
             
             
         }
-        rl.question('Woud you like to an add array? or search?\n(add/search) Input: ', (answer) => {
+        rl.question('\n\nWoud you like to an add array? or search?\n(add/search) Input: ', (answer) => {
             answer = answer.trim().toLowerCase().substring(0,1);
             if (answer == 'a') {
                 console.log('You selected add another array...\n\n');
                 add_array();
 
-            } else {
+            } else if (answer == 's') {
                 console.log('bringing you to the search...');
                 search_arrays();
+
+            } else {
+                console.log('Your selection was invalid, you will add another array.');
+                add_array();
+                
             }
         })
         
@@ -36,13 +41,38 @@ add_array()
 
 function search_arrays() {
 
-    console.log('The available arrays to search in are...'\n______________________________________);
+    console.log('The available arrays to search in are...\n______________________________________');
     for (let i = 0; i < allArrays.length; i++) {
         console.log(i+1 + ": ", allArrays[i]);
     }
     console.log('________________________________________________\n');
-    
-    
-//NEXT STEP add ability to select any number of arrays and search for the largest number in all that were selected
 
+
+    rl.question('Select the array/arrays you would like to search in by typing in the digit next to the array in the array list above.\n\nExample: 1,3 would select the first and third array if my array list was long enough,\n then return the largest number of both arrays to the console.\nInput: ', (arrayNums) => {
+        arrayNums = arrayNums.trim().replace(/ /g, ',').split(',');
+        let largestNum = Number.NEGATIVE_INFINITY;
+        for (let i = 0; i < allArrays.length; i++) {
+            for (let j = 0; j < arrayNums.length; j++) {
+                if (arrayNums[j] == i + 1) {
+                    for (let k = 0; k < allArrays[i].length; k++) {
+
+                        if (allArrays[i][k] > largestNum) {
+                            largestNum = allArrays[i][k];
+                        }
+                    }  
+                }
+            } 
+        }
+        console.log('\n\nThe largest number in all arrays chosen was ( ' + largestNum, ")");
+        rl.question('\nWould you like to add to your list of arrays?\(anything but \'add\'\ will exit the program.\nInput: ', (answer) => {
+            answer = answer.trim().substring(0,3).toLowerCase();
+
+            if (answer == 'add') {
+                add_array()
+            } else {
+                rl.close()
+            }
+
+        })
+    }) 
 }
