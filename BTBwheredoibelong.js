bee4c0bc5749cfcefa3a93109175b7f044c3fd33
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-26 14:57:27 -0500

    Add files via upload

--- BTBwheredoibelong.js
+++ BTBwheredoibelong.js
@@ -1,21 +1,33 @@
-var readline = require('readline'),
+const readline = require('readline'),
     rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
 
+    let listOfNumbers = [];
+add_to_list()
 function add_to_list() {
-    rl.question('Add a number to the list, or input "done" to find placement for a new number\nInput: ' (addto) => {
-        addto = parseInt(addto);
+    rl.question('Add a number to the list, or input "done" to find placement for a new number\nInput: ', (addto) => {
 
-        if (!isNaN(addto)) {
-            
-        } else if (addto = 'done') {
+        if (addto == 'done') {
             find_placement()
+        }
+
+        addto = parseInt(addto);
+        if (!isNaN(addto)) {
+
+            listOfNumbers.push(addto);
+            add_to_list()
+            
+        } else {
+            console.log('That is not a number, try again\n\n');
+            add_to_list()
         }
     })
 }
 
 function find_placement() {
+    listOfNumbers.sort((a,b) => {return a-b})
+    console.log(listOfNumbers);
     rl.question()
 }
