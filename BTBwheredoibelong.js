62f4968a04aba93426f889384f92b4aaf9461898
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-26 14:50:35 -0500

    Add files via upload

--- BTBwheredoibelong.js
+++ BTBwheredoibelong.js
@@ -5,5 +5,17 @@ var readline = require('readline'),
     });
 
 function add_to_list() {
-    
+    rl.question('Add a number to the list, or input "done" to find placement for a new number\nInput: ' (addto) => {
+        addto = parseInt(addto);
+
+        if (!isNaN(addto)) {
+            
+        } else if (addto = 'done') {
+            find_placement()
+        }
+    })
+}
+
+function find_placement() {
+    rl.question()
 }
