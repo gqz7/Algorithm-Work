6da1281a4ed5cf0788046ed3a4387d4281bb91a1
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2019-11-27 02:39:08 -0500

    Add files via upload
    one bug found; when choosing placement number an automatic entry of blank space in entered after first entry into second to last function, this gives 'try again' message to a user that has not yet inputted anything for this question.

--- BTBwheredoibelong.js
+++ BTBwheredoibelong.js
@@ -4,30 +4,77 @@ const readline = require('readline'),
         output: process.stdout
     });
 
-    let listOfNumbers = [];
+    let listOfNumbers = [], index;
+
 add_to_list()
 function add_to_list() {
-    rl.question('Add a number to the list, or input "done" to find placement for a new number\nInput: ', (addto) => {
+    rl.question('\n\nAdd a number to the list, or input "done" to find placement for a new number\nAll inputs will be saved as integers\nInput: ', (addto) => {
 
         if (addto == 'done') {
             find_placement()
+        } else {
+            addto = parseInt(addto);
         }
-
-        addto = parseInt(addto);
+        
         if (!isNaN(addto)) {
 
             listOfNumbers.push(addto);
             add_to_list()
             
         } else {
-            console.log('That is not a number, try again\n\n');
+            console.log('\nThat is not a number, try again\n');
             add_to_list()
         }
     })
 }
 
 function find_placement() {
-    listOfNumbers.sort((a,b) => {return a-b})
-    console.log(listOfNumbers);
-    rl.question()
+
+
+    // console.log(`\n____________________________________________________\n\n${listOfNumbers}\n____________________________________________________\n`);
+    rl.question('Enter a number and we will find its place in the list\nNumber: ', (placementNum) => {
+        let isItANum = (placementNum);
+            isItANum = parseInt(isItANum);
+
+        if (!isNaN(isItANum)) {
+  
+                listOfNumbers.push(placementNum);
+                listOfNumbers.sort((a,b) => {return a-b});
+                index = listOfNumbers.indexOf(placementNum);
+
+            console.log('\n\n\n\n\n');
+            result()
+
+        } else {
+            console.log('Your input was not a number, please try again...\n\n');
+            find_placement()
+            
+        }
+    })
+}
+
+function result() {
+    
+    rl.question('Would you like to display your full array with the new number in place,\n OR would you like to know the index your new number is at in the array?\n(or "end" the program)\n\n(display/index/end)\nInput: ', (answer) => {
+        answer = answer.trim().toLowerCase().substring(0,1);
+
+        if (answer == 'd') {
+
+            console.log(`(your new number is highlighted in green)\nYour New Array;\n____________________________________________________________\n\n`, listOfNumbers, `\n____________________________________________________________`);
+            result()
+
+        } else if (answer == 'i') {
+
+            console.log(`Your new number was at index ${index}!`);
+            result()
+
+        } else if (answer == 'e') {
+            rl.close()
+            
+        } else {
+            console.log('This is not a valid entry, please try agian...\n\n\n');
+            result()
+            
+        }
+    })
 }
