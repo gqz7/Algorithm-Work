4c22b370c3591837b1eb02560361f813331ae326
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-01 23:42:40 -0500

    now trying to navigate d/m/y

--- calendarnav.js
+++ calendarnav.js
@@ -1,12 +1,17 @@
 let computerDate, dd, mm, yyyy, today, currentDate;
 
+const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+
     window.onload = () => {
 
         computerDate = new Date(),
-        dd = String(computerDate.getDate()).padStart(2, '0'),
-        mm = String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0!
-        yyyy = computerDate.getFullYear(),
-        today = yyyy + '-' + mm + '-' + dd,
+        dd = '31',
+        //String(computerDate.getDate()).padStart(2, '0'),
+        mm = '12',
+        // String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0!
+        yyyy =  '2000',
+        // computerDate.getFullYear(),
+        today,
         currentDate = today;
 
         set_date()
@@ -15,25 +20,25 @@ let computerDate, dd, mm, yyyy, today, currentDate;
 
     
     function set_date() {
+        today = yyyy + '-' + mm + '-' + dd,
         document.getElementById('date').innerHTML = today;
     }
 
     document.getElementById('nxtday').addEventListener('click', next_day);
     document.getElementById('prvday').addEventListener('click', previous_day);
 
-    function daysInMonth(month, year) {
-        return 32 - new Date(year, month, 32).getDate();
-    }
-  
+
     function next_day() {
-
-        console.log(today);
-
-        if (parseInt(dd) + 1 > daysInMonth(mm,yyyy) && mm < 12) {
-            today = yyyy + '-' + (parseInt(mm) + 1) + '-' + '01';
+       
+        if (parseInt(dd) + 1 > 31 && mm < 12) {
+            mm = "0" + (parseInt(mm) + 1);
+            dd = "01";
         } else if (parseInt(dd) + 1 > 31 && mm == 12) {
-            today = (parseInt(yyyy) + 1) + '-' + 01 + '-' + '01';
-        } else if (parseInt(dd) + 1 < daysInMonth(mm,yyyy)) {
+            // today = (parseInt(yyyy) + 1) + '-' + 01 + '-' + '01';
+            yyyy = (parseInt(yyyy) + 1);
+            mm = '01';
+            dd = '01';
+        } else if (parseInt(dd) + 1 < 31) {
 
             if ((parseInt(dd) + 1 < 10)) {
                 today = yyyy + '-' + mm + '-0' + (parseInt(dd) + 1);
