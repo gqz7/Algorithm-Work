107ffd10e796aa9951e15d114456011e922d7022
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-02 10:56:44 -0500

    next day button works
    working on next month and next year functions

--- calendarnav.js
+++ calendarnav.js
@@ -1,17 +1,32 @@
+//initalize variables
 let computerDate, dd, mm, yyyy, today, currentDate;
 
 const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
 
+//event listeners
+    //day
+    document.getElementById('nxtday').addEventListener('click', next_day);
+    document.getElementById('prvday').addEventListener('click', previous_day);
+    //month
+    document.getElementById('prvday').addEventListener('click', previous_day);
+    document.getElementById('prvday').addEventListener('click', previous_day);
+    //year
+    document.getElementById('prvday').addEventListener('click', previous_day);
+    document.getElementById('prvday').addEventListener('click', previous_day);
+
+//functions
     window.onload = () => {
 
         computerDate = new Date(),
-        dd = '31',
+        dd = '28',
         //String(computerDate.getDate()).padStart(2, '0'),
-        mm = '10',
-        // String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0!
+
+        mm = '08',
+        // String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0 so add 1 to .getMonth()
+        
         yyyy =  '2000',
         // computerDate.getFullYear(),
-        today,
+
         currentDate = computerDate;
 
         set_date()
@@ -21,7 +36,7 @@ const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
     
     function set_date() {
         today = yyyy + '-' + mm + '-' + dd;
-        document.getElementById('date').innerHTML = today;
+        document.getElementById('date').innerText = today;
     }
 
     document.getElementById('nxtday').addEventListener('click', next_day);
@@ -30,23 +45,22 @@ const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
 
     function next_day() {
 
-        console.log(daysInMonth[parseInt(mm)-1], currentDate);
-        
+        console.log(daysInMonth[mm-1]);
        
-        if (parseInt(dd) + 1 > daysInMonth[parseInt(mm)-1] && mm < 12) {
-            mm = "0" + (parseInt(mm) + 1);
+        if (parseInt(dd) + 1 > daysInMonth[mm-1] && mm < 12) {
+            mm = next_month(mm);
             dd = "01";
-        } else if (parseInt(dd) + 1 > daysInMonth[parseInt(mm)-1] && mm == 12) {
+        } else if (parseInt(dd) + 1 > daysInMonth[mm-1] && mm == 12) {
             // today = (parseInt(yyyy) + 1) + '-' + 01 + '-' + '01';
             yyyy = (parseInt(yyyy) + 1);
             mm = '01';
             dd = '01';
-        } else if (parseInt(dd) + 1 < daysInMonth[parseInt(mm)-1]) {
+        } else if (parseInt(dd) + 1 <= daysInMonth[mm-1]) {
 
             if ((parseInt(dd) + 1 < 10)) {
-                today = yyyy + '-' + mm + '-0' + (parseInt(dd) + 1);
+                dd = "0" + (parseInt(dd) + 1);
             } else {
-                today = yyyy + '-' + mm + '-' + (parseInt(dd) + 1);
+                dd = (parseInt(dd) + 1);
             }
            
         }
@@ -59,7 +73,13 @@ const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
         
     }
 
-    function next_month(){
+    function next_month(mm){
+
+        if (mm < 9) {
+            return "0" + (parseInt(mm)+1);
+        } else {
+            return (parseInt(mm)+1);
+        }
 
     }
 
