225e1a1f01d75a2812824a5579cdfefb7b6d118c
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-01 23:56:43 -0500

    bug fixes

--- calendarnav.js
+++ calendarnav.js
@@ -1,18 +1,18 @@
 let computerDate, dd, mm, yyyy, today, currentDate;
 
-const months = [31,28,31,30,31,30,31,31,30,31,30,31];
+const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
 
     window.onload = () => {
 
         computerDate = new Date(),
         dd = '31',
         //String(computerDate.getDate()).padStart(2, '0'),
-        mm = '12',
+        mm = '10',
         // String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0!
         yyyy =  '2000',
         // computerDate.getFullYear(),
         today,
-        currentDate = today;
+        currentDate = computerDate;
 
         set_date()
 
@@ -20,7 +20,7 @@ const months = [31,28,31,30,31,30,31,31,30,31,30,31];
 
     
     function set_date() {
-        today = yyyy + '-' + mm + '-' + dd,
+        today = yyyy + '-' + mm + '-' + dd;
         document.getElementById('date').innerHTML = today;
     }
 
@@ -29,16 +29,19 @@ const months = [31,28,31,30,31,30,31,31,30,31,30,31];
 
 
     function next_day() {
+
+        console.log(daysInMonth[parseInt(mm)-1], currentDate);
+        
        
-        if (parseInt(dd) + 1 > 31 && mm < 12) {
+        if (parseInt(dd) + 1 > daysInMonth[parseInt(mm)-1] && mm < 12) {
             mm = "0" + (parseInt(mm) + 1);
             dd = "01";
-        } else if (parseInt(dd) + 1 > 31 && mm == 12) {
+        } else if (parseInt(dd) + 1 > daysInMonth[parseInt(mm)-1] && mm == 12) {
             // today = (parseInt(yyyy) + 1) + '-' + 01 + '-' + '01';
             yyyy = (parseInt(yyyy) + 1);
             mm = '01';
             dd = '01';
-        } else if (parseInt(dd) + 1 < 31) {
+        } else if (parseInt(dd) + 1 < daysInMonth[parseInt(mm)-1]) {
 
             if ((parseInt(dd) + 1 < 10)) {
                 today = yyyy + '-' + mm + '-0' + (parseInt(dd) + 1);
@@ -53,6 +56,22 @@ const months = [31,28,31,30,31,30,31,31,30,31,30,31];
     }
 
     function previous_day() {
+        
+    }
+
+    function next_month(){
+
+    }
+
+    function previous_month(){
+        
+    }
+
+    function next_year(){
+        
+    }
+
+    function previous_year(){
         
     }
 
