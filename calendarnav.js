8f5c54cf22a9e767187e654662a260122d89b384
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-03 23:18:40 -0500

    bug fix, nxt/prv day functions

--- calendarnav.js
+++ calendarnav.js
@@ -17,81 +17,70 @@ const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
 //functions
     window.onload = () => {
 
-        computerDate = new Date(),
-        dd = '04',
-        //String(computerDate.getDate()).padStart(2, '0'),
-
-        mm = '01',
-        // String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0 so add 1 to .getMonth()
-        
-        yyyy =  '2000',
-        // computerDate.getFullYear(),
-
-        currentDate = computerDate;
-
+        computerDate = new Date(), //creates a date objects when the screen loads
+        dd = String(computerDate.getDate()).padStart(2, '0'), //gets all the neccecary data from the new Date obj
+        mm = String(computerDate.getMonth() + 1).padStart(2, '0'), //January is 0 so add 1 to .getMonth()
+        yyyy =  computerDate.getFullYear(), 
+        currentDate = yyyy + '-' + mm + '-' + dd; //stores the current date and this will not be changed unless the page is reloaded
         set_date()
 
     }
 
     
-    function set_date() {
+    function set_date() { //this function will be called onload, aswell as every time the a button is pressed
 
-        console.log("test");
+        // console.log("test");
 
         today = yyyy + '-' + mm + '-' + dd;
         document.getElementById('date').innerText = today;
     }
 
-    // document.getElementById('nxtday').addEventListener('click', next_day);
-    // document.getElementById('prvday').addEventListener('click', previous_day);
-
-
     function next_day() { //WORKING
        
-        if (parseInt(dd) + 1 > daysInMonth[mm-1] && mm < 12) {
+        if (parseInt(dd) + 1 > daysInMonth[mm-1] && mm < 12) { //if its going into the next month, but within the same year
 
             mm = mm < 9 ? "0" + (parseInt(mm)+1) : (parseInt(mm)+1);
             dd = "01";
 
-        } else if (parseInt(dd) + 1 > daysInMonth[mm-1] && mm == 12) {
+        } else if (parseInt(dd) + 1 > daysInMonth[mm-1] && mm == 12) { //if its the last day of the year
             
             yyyy = (parseInt(yyyy) + 1);
             mm = '01';
             dd = '01';
 
-        } else if ( + 1 <= daysInMonth[mm-1]) {
+        } else if ( parseInt(dd) + 1 <= daysInMonth[mm-1]) { //if it is not the end of the month
 
-            if ((parseInt(dd) + 1 < 10)) {
+            if ((parseInt(dd) + 1 < 10)) { //if the date is less than 10 a 0 must be placed before the digit
                 dd = "0" + (parseInt(dd) + 1);
 
-            } else {
+            } else { //if it is greater or equal to 10 the format will be correct without addind the '0'
                 dd = (parseInt(dd) + 1);
 
             }
            
         }
 
-        set_date()
+        set_date() //sets the date with changed values
 
     }
 
     function previous_day() { //WORKING
 
-        if (parseInt(dd) - 1 === 0 && mm > 1) {
+        if (parseInt(dd) - 1 === 0 && mm > 1) { //if it is the first of the month, the next day back will be the last day of the previous month
 
-            mm = mm < 9 ? "0" + (parseInt(mm)-1) : (parseInt(mm)-1);
-            dd = daysInMonth[mm-1];
-            console.log(daysInMonth[mm-1]);
+            mm = mm < 9 ? "0" + (parseInt(mm)-1) : (parseInt(mm)-1); //this ternary operator makes sure the months are correctly formated
+            dd = daysInMonth[mm-1]; 
+            // console.log(daysInMonth[mm-1]); //this will return the total number of days in the previous month
             
-        } else if (parseInt(dd) - 1 === 0 && mm == 1) {
+        } else if (parseInt(dd) - 1 === 0 && mm == 1) { //if is is January 1st then the date will be Dec. 31st on the year prior
 
             yyyy = (parseInt(yyyy) - 1);
             mm = '12';
             dd = '31';
 
-        } else if ( + 1 <= daysInMonth[mm-1]) {
+        } else if (parseInt(dd) - 1 > 0) { //if it is any other day of the month but the first
 
-            if ((parseInt(dd) - 1 < 10)) {
+            if ((parseInt(dd) - 1 < 10)) { //makes sure day formating is correct 
                 dd = "0" + (parseInt(dd) - 1);
 
             } else {
@@ -101,27 +90,27 @@ const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
            
         }parseInt(dd)
 
-        set_date()
+        set_date() //sets the date with changed values
         
     }
 
     function next_month(){ //WORKING
 
-        if (parseInt(mm) + 1 > 12) {
+        if (parseInt(mm) + 1 > 12) { //if the next month is the new year
             yyyy = (parseInt(yyyy) + 1);
             mm = '01';
             
-        } else if (parseInt(mm) + 1 <= 12 && parseInt(mm) + 1 > 9) {
+        } else if (parseInt(mm) + 1 <= 12 && parseInt(mm) + 1 > 9) { //if next month is still within the calendar year
             mm = (parseInt(mm)+1);
-        } else {
+        } else {  //these conditions assure corret formating
             mm = "0" + (parseInt(mm)+1);
         } 
 
-        set_date()
+        set_date() //sets the date with changed values
 
     }
 
-    function previous_month(){
+    function previous_month(){ //WORKING
 
         if (parseInt(mm) - 1 < 1) {
             yyyy = (parseInt(yyyy) - 1);
@@ -133,23 +122,23 @@ const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
             mm = "0" + (parseInt(mm)-1);
         } 
 
-        set_date()
+        set_date() //sets the date with changed values
         
     }
 
-    function next_year(){
+    function next_year(){ //WORKING
 
-        yyyy = (parseInt(yyyy) + 1);
-
-        set_date()
+        yyyy = (parseInt(yyyy) + 1); //very simple function but the '+=' can not be used because it is a string variable
+ 
+        set_date() //sets the date with changed values
         
     }
 
-    function previous_year(){
+    function previous_year(){ //WORKING
 
-        yyyy = (parseInt(yyyy) - 1);
+        yyyy = (parseInt(yyyy) - 1); //the variable must be parsed before the operator takes place.
 
-        set_date()
+        set_date() //sets the date with changed values
         
     }
 
