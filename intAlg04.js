0c79ada62c4e3c5b3e45df2fab72fe5e00ea745d
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-12 10:45:32 -0500

    wherefore art thou

---
+++ intAlg04.js
@@ -0,0 +1,8 @@
+const readline = require('readline'),
+      rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+      });
+
+
+rl.question('Enter an array of objects')
