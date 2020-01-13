f78d65eb306b8f2b3dd359209353ff74bc090693
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-13 16:56:29 -0500

    the framework of my object storage cli

--- intAlg04.js
+++ intAlg04.js
@@ -2,7 +2,45 @@ const readline = require('readline'),
       rl = readline.createInterface({
           input: process.stdin,
           output: process.stdout
-      });
+      }),
+
+      lineBr = '_______________________________________________';
 
 
-rl.question('Enter an array of objects')
+console.log(lineBr + '\nWelcome to Javascript Object Storage\n' + lineBr);
+menu()
+
+
+
+
+function menu() {
+    
+    rl.question('Would you like to...\n1). Search for an object\n2). Create an object\n3). Take inventory of object\n4). Exit the program\n\nEnter a number for selection: ',
+    (ans) => {
+
+        switch (ans) {
+            case '1':
+
+            break;
+            case '2':
+
+            break;
+            case '3':
+
+            break;
+            case '4':
+                ETP()
+            break;
+            
+
+        }
+    })
+}
+
+
+function ETP() {
+
+    console.log('\n\nThe program will now exit...');
+    rl.close()
+    
+}
