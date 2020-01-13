db13ff21b092e20a77fa9b674884800e5955fd7f
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-13 16:57:00 -0500

    init commit for piglatin algorithm

---
+++ intAlg06.js
@@ -0,0 +1,15 @@
+// If a word begins with a vowel you just add "way" to the end.
+
+// If a word does not contain a vowel, just add "ay" to the end.
+
+// else take the first letter at ay to it and attach that to the end of the word
+
+const readline = require('readline'),
+      rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+      });
+
+rl.question('Enter a string and it will be converted into piglatin for you\nInput: ', (string) => {
+    
+})
