8d0b5ee8d3c9e2f64a0b943824518e7c09845271
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-13 21:32:56 -0500

    piglatin working

--- intAlg06.js
+++ intAlg06.js
@@ -8,8 +8,54 @@ const readline = require('readline'),
       rl = readline.createInterface({
           input: process.stdin,
           output: process.stdout
-      });
+      }),
+      vowels = /[aeiou]/;
 
 rl.question('Enter a string and it will be converted into piglatin for you\nInput: ', (string) => {
+
+    words = string.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+
+        // console.log(vowels.test(words[i][0]));
+        
+        words[i] = piglatinConvert(words[i]);
+
+        // console.log(words[i]);
+        
+    }
+
+    words = words.join(' ')
     
+    //Result
+    console.log(`\n\n\nYour Piglatin Phrase is:\n--------------------------\n${words}`);
+    
+
+    rl.close()
 })
+
+
+function piglatinConvert(enWord) {
+
+    if (vowels.test(enWord[0])) {
+
+        return enWord = enWord+'way';
+        
+    } else if (vowels.test(enWord)) {
+
+        for(let i = 0; i < enWord.length; i++) {
+            if (vowels.test(enWord[i])) {
+
+                return enWord = enWord.substring(i, enWord.length) + enWord.substring(0, i) + 'ay';
+
+            }
+            
+        }
+
+    } else {
+
+        enWord = enWord+'ay';
+        return enWord
+    }
+    
+}
