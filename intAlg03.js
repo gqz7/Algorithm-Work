441c73f19f3acb2864237a9fb30b349f65a3f6a5
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-14 07:19:05 -0500

    search and destroy
    adding in end case if final array is empty, there are also formatting instructions in beginning.

--- intAlg03.js
+++ intAlg03.js
@@ -4,11 +4,20 @@ const readline = require('readline'),
       rl = readline.createInterface({
           input: process.stdin,
           output: process.stdout
-      });
+      }),
+
+      lb = '__________________________________';
+
+console.log(`${lb}\n\nARRAY FORMAT --- [1,2,3] or "4 5 6"\n${lb}\n`);
+
 
 rl.question('Enter an array\ninput: ', (arr) => {
     arr = arr.replace(/\[|\]|\"|\'|/g, '').replace(/ /g, ',').split(',');
 
+    arr = arr.filter((elm) => elm != '' );
+
+    console.log(`Your array: ${arr}`);
+    
     
     rl.question('\nNow Enter a value or an array of values\nthoes that match the value in the current array will be destroyed\ninput: ', (destroyArr) => {
         destroyArr = destroyArr.replace(/\[|\]|\"|\'|/g, '').replace(/ /g, ',').split(',');
@@ -21,7 +30,10 @@ rl.question('Enter an array\ninput: ', (arr) => {
             }
         }
 
-        console.log(`\n_____________________________\nYour final array is...\n\n${arr}\n\nThank you for using the program...\n`);
+        let message = arr.length != 0 ? 'Your final array is' : 'Your array is now empty';
+            
+
+        console.log(`\n\n${message}...\n${lb}\n\n${arr}\n${lb}\nThank you for using the program...\ncreated by gqs`);
         
         rl.close()
     })
