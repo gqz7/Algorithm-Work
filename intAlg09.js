b65f2898a870a343bf0d77eeae5f09434ddee03e
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-17 14:23:29 -0500

    final commit for missing character finder
    this program works by taking user input via the 'readline' module. then it splits the string into an array of single characters. this array is then sorted which makes finding the first and last num/letter-char a lot easier. if numbers are present the 0th index will always be the smallest number and if letters are present the last index will always be the closest to the end of the alphabet. some basic logic is used to set the 4 params need, numStart/End and alphaStart/End. these params are passed to two functions for finding the missing characters. these functions are only called if their respective char type is present in the user's input. when these functions run they iterate a for-loop where i is initialized as the 'start' variable's charcode. and the loop ends when i reaches the 'end' variable's charcode. the string.fromCharCode is checked to be present in the users array. if not found that char is added to the respective 'missingArray'. finally the display function will be called to show the user what characters were missing

--- intAlg09.js
+++ intAlg09.js
@@ -1,15 +1,18 @@
 //alphanumberic missing chars
 
+//VARIABLE DECLERATION
 const readline = require('readline'),
       rl = readline.createInterface({
           input: process.stdin,
           output: process.stdout
       });
 
-let missingAlpha = [], missingNums = [];
+let missingAlpha = [], missingNums = [], stringArr, numStart, numEnd, alphaStart, alphaEnd;
 
+//intro
 console.log('Enter a string with a portion of the alphabet and/or a portion of the decimal number sequence.\nYour output will show the missing characters\n');
 
+//SET FUNCTION
 set_ranges()
 
 function set_ranges() {
@@ -18,24 +21,24 @@ function set_ranges() {
 
         stringArr = string.trim().toLowerCase().replace(/\W/g, '').split('').sort();
 
-        console.log(stringArr);
+        // console.log(stringArr);
 
         //check if the string provided is long enough to work with
         if (stringArr.length > 1) {
 
-            let numStart = !isNaN(parseInt(stringArr[0])) ? stringArr[0] : null,
-            numEnd, alphaStart, alphaEnd;
-
-            
+            numStart = !isNaN(parseInt(stringArr[0])) ? stringArr[0] : null;            
 
             //define the start and end points for the number/alpha sequences provided
             if (numStart != null) {
                 
                 for (let i = 0; i < stringArr.length; i++) {
+
                     if (isNaN(parseInt(stringArr[i]))) {
+
                         numEnd = stringArr[i-1];
                         alphaStart = stringArr[i];
                         alphaEnd = stringArr[stringArr.length-1];
+
                         i = stringArr.length
                     }
                     
@@ -43,24 +46,26 @@ function set_ranges() {
             } else {
                 
                 alphaStart = stringArr[0];
-                alphaEnd = stringArr[stringArr.length-1];
 
+                alphaEnd = stringArr[stringArr.length-1];
                 
             }
 
-            console.log(numStart, numEnd, alphaStart, alphaEnd);
+            // console.log(numStart, numEnd, alphaStart, alphaEnd);
             
             if (numStart != null) {
                 find_missing_nums(numStart, numEnd)
+                
             }
 
             if (alphaStart != undefined) {
                 find_missing_alpha(alphaStart, alphaEnd)
+                
             }
-            
-            rl.close()
+
+            display()
         
-        //if the user entered a string too short
+        //if the user entered a string too short they are prompted to try again
         } else {
 
             console.log('The string provided was not long enough to work with, please try again...\n\n');
@@ -72,13 +77,69 @@ function set_ranges() {
     
 }
 
+//Finds the missing alphabet characters in the users input
 function find_missing_alpha(start, end) {
 
+    for (let i = start.charCodeAt(0) + 1; i < end.charCodeAt(0); i++) {
+        
+        // console.log(i);
+
+        if (!stringArr.includes(String.fromCharCode(i))) {
+
+            missingAlpha.push(String.fromCharCode(i));
+
+        }
+    
+    }
+
 }
 
-
+//Finds the missing numbers in the users input
 function find_missing_nums(start, end) {
+
+    for (let i = start.charCodeAt(0) + 1; i < end.charCodeAt(0); i++) {
+        // console.log(i);
+
+        if (!stringArr.includes(String.fromCharCode(i))) {
+
+            missingNums.push(String.fromCharCode(i));
+
+        }
+        
+    }
     
 }
 
+function display() {
+
+    console.log('\n_____________________________________________________________________\nResults');
     
+    //display number results
+    if (missingNums.length > 0) {
+
+        console.log(`\n\nYour sequence started with ${numStart} and ended with ${numEnd},\nyou were missing: ${missingNums}\n`);
+        
+    } else if (alphaStart != undefined) {
+
+        console.log('\nYou were not missing any numbers\n');
+
+    } 
+
+    //display alphabet results
+
+    if (missingAlpha.length > 0) {
+
+        console.log(`\nYour sequence started with ${alphaStart} and ended with ${alphaEnd},\nyou were missing: ${missingAlpha}\n`);
+        
+    }  else if (alphaStart != undefined) {
+
+        console.log('\nYou were not missing any letters of the alphabet\n');
+
+    } 
+
+    //end program
+    console.log('Program closing...');
+    
+    rl.close()
+    
+}  
