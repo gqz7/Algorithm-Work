aa32ee1a6f75bdd3031103319097a1cbf876bbac
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-18 21:45:04 -0500

    intial commit for fibonnaci algorithm
    set up functions and necessary variables

---
+++ intAlg11.js
@@ -0,0 +1,24 @@
+//fibonacci numbers 113581321
+
+//GLOBAL VARS
+const readline  = require('readline'),
+      rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+      });
+
+let fibArr = [];
+
+select_range()
+
+function select_range() {
+    
+}
+
+function select_operation() {
+    
+}
+
+function nav() {
+    
+}
