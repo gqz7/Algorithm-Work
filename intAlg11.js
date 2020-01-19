3bc234a144517bd2a69af2575dbb47b34b6a257b
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-18 23:14:39 -0500

    creation of fibonacci sequence array working

--- intAlg11.js
+++ intAlg11.js
@@ -12,6 +12,39 @@ let fibArr = [];
 select_range()
 
 function select_range() {
+
+    rl.question('\nEnter an endpoint for your fibonacci sequence.\nNumber: ', (num) => {
+
+        num = parseInt(num.trim())
+
+        if (!isNaN(num) && num < 1000000000000 && num > 1) {
+
+            let fib0 = 0, fib1 = 1, fib2 = 1;
+
+            while (fib2 <= num) {
+                fibArr.push(fib2);
+
+                fib2 = fib1 + fib0;
+
+                fib0 = fib1;
+                
+                fib1 = fib2
+
+                console.log(fib2);
+                
+            }
+
+            console.log(fibArr);
+            
+                
+        
+            
+        } else {
+            console.log('That was not a vaild number, try again');
+            select_range()
+            
+        }
+    })
     
 }
 
