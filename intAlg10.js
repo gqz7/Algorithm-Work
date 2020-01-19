556c770185e626c3396ab344d122034e83614468
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-18 20:47:14 -0500

    update add function, implement naviation
    improved how non-numeric characters are filtered out of the users input, last step is to complete the navigation function

--- intAlg10.js
+++ intAlg10.js
@@ -13,28 +13,53 @@ add_to_union()
 function add_to_union() {
     
     rl.question('Enter an array of numbers, they will be added to the union array.\nARRAY FORMAT --- [1,2,3] or "4 5 6"\n\ninput:', (arr) => {
-        
-        arr = arr.trim().toLowerCase().replace(/ /g, ',').replace(/[a-z]/g, '').split(',');
 
-        // console.log(arr);
-        
-        for (let i = 0; i < arr.length; i++) {
-
-            arr[i] = parseInt(arr[i]);
-
-            if (!union.includes(arr[i]) && !isNaN(arr[i])) {
-                union.push(arr[i])
-            }
+        //puts the string given by the user into an array format and excludes all special symbols and letters with a filter method
+        arr = arr.trim().toLowerCase().replace(/ /g, ',').split(',').filter(element => {
             
+            if (!isNaN(parseInt(element))) { //only numbers will pass true
+
+                return true
+
+            } else {
+
+                return false
+            }
+
+        });
+
+        //checks to make sure theres at least A number to check for in the union array
+        if (arr.length > 0) {
+
+            for (let i = 0; i < arr.length; i++) {
+
+                arr[i] = parseInt(arr[i]);
+                
+                //if the union array does not have the number in the current index of arr then it will be pushed to union
+                if (!union.includes(arr[i])) {
+                    union.push(arr[i])
+                }
+                
+            }
+    
+            //Display to the user their current array, then inquire what they'd like to do next
+            console.log(`Thank you for your input,\n your current sorted union...\n\n${union}\n`);
+            nav()
+            
+        } else {
+
+            console.log('That was not a vaild input, please try again...');
+            
+            add_to_union()
+
         }
-
-        // console.log(union);
-
-        add_to_union()
         
     })
 }
 
 function nav() {
+
+    
+    
     
 }
