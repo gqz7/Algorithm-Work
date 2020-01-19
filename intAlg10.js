398fe95ae0cd7aeb4250f34f905157160292e7eb
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-18 21:11:04 -0500

    sorted union algorthim

--- intAlg10.js
+++ intAlg10.js
@@ -1,5 +1,6 @@
 //sorted union
 
+//GLOBAL VARS
 const readline  = require('readline'),
       rl = readline.createInterface({
           input: process.stdin,
@@ -8,14 +9,23 @@ const readline  = require('readline'),
 
 let union = [];
 // Task, allow user to add arrays to a union, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+//INTRO 
+
+console.log('Enter an array of numbers, they will be added to the union array.\nARRAY FORMAT --- [1,2,3] or "4 5 6"\n');
+
 add_to_union()
 
+
+//FUNCTIONS
+
+//ADD VALUES TO THE UNION
 function add_to_union() {
     
-    rl.question('Enter an array of numbers, they will be added to the union array.\nARRAY FORMAT --- [1,2,3] or "4 5 6"\n\ninput:', (arr) => {
+    rl.question('\nAdd to Union Array\nArray:', (arr) => {
 
         //puts the string given by the user into an array format and excludes all special symbols and letters with a filter method
-        arr = arr.trim().toLowerCase().replace(/ /g, ',').split(',').filter(element => {
+        arr = arr.trim().toLowerCase().replace(/ /g, ',').replace(/\[|\]/g ,'').split(',').filter(element => {
             
             if (!isNaN(parseInt(element))) { //only numbers will pass true
 
@@ -43,13 +53,13 @@ function add_to_union() {
             }
     
             //Display to the user their current array, then inquire what they'd like to do next
-            console.log(`Thank you for your input,\n your current sorted union...\n\n${union}\n`);
+            console.log(`\nThank you for your input,\nyour current sorted union...\n\n   ${union}\n`);
             nav()
             
         } else {
 
             console.log('That was not a vaild input, please try again...');
-            
+
             add_to_union()
 
         }
@@ -57,9 +67,34 @@ function add_to_union() {
     })
 }
 
+
+//NAVIGATION MENU
 function nav() {
 
-    
+    rl.question('Would you like to...\n1. Add to union\n2. Clear union data\n3. Exit the program\nNumber Selection: ', (answer) => {
+        switch (answer) {
+            case '1': add_to_union()
+            break
+
+            case '2': union = [];
+
+                      console.log('______________\nUnion Cleared\n______________\n\nAdd to Array...');
+                      
+                      add_to_union()
+            break
+            
+            case '3': 
+                        console.log('Thank you for using the program, it will now close...');
+                        
+                        rl.close();
+            break
+
+            default:
+                console.log('That was not a vaild input...\n');
+                nav()
+                
+        }
+    })
     
     
 }
