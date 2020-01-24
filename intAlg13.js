fae8fd55dc7bf70e4427746c59f4ed0d8ca8f456
GabrielQSherman <56276327+GabrielQSherman@users.noreply.github.com>
2020-01-23 23:21:39 -0500

    lcm with prime factors
    using prime factorization method to get lcm

--- intAlg13.js
+++ intAlg13.js
@@ -6,17 +6,66 @@ const readline = require('readline'),
           output: process.stdout
       });
 
+let primeFactorArray;
+
 console.log('enter two or more numbers seperated by spaces or commas. then the output will be the Least Common Multiple of thoes nubers');
 
-enter_factors()
+find_lcm()
 
-function enter_factors() {
+function find_lcm() {
     
     
     rl.question('Numbers: ', (numbers) => {
         factors = numbers.replace(/ /g, ',').split(',').filter(num => {return !isNaN(parseInt(num))});
 
-        console.log(factors);
+        primeFactorArray = []
+
+        for (let i = 0; i < factors.length; i++) {
+
+            let num = factors[i];
+            
+           factorialize(num);
+            
+        }
+
+        let lcm = primeFactorArray.reduce((a,b) => {return a*b});
+
+        console.log(primeFactorArray);
+
+        console.log(`The Least Common Factor of ${primeFactorArray} is ${lcm}`);
+        
         
     })
 }
+
+
+function factorialize(num) {
+    
+
+    let primeFactor = 2,
+        pFactorsArr = [];
+
+    while (primeFactor <= num) {
+
+        console.log(num,  primeFactor, primeFactorArray);
+
+        if (num % primeFactor == 0) {
+            primeFactorArray.push(primeFactor);
+
+            if (num / primeFactor == 1) {
+
+                primeFactor = num + 1;
+                
+            } else {
+
+                num = num / primeFactor;
+                
+            }
+
+        } else {
+            primeFactor++
+        }
+        
+    }
+
+} 
