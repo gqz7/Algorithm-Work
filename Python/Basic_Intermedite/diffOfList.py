6c002f00eafda74061c30987d38e37fb7d25a0c2
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-12 20:58:45 -0400

    update deliverly of answer to algorithm

--- Python/Basic_Intermedite/diffOfList.py
+++ Python/Basic_Intermedite/diffOfList.py
@@ -1,4 +1,6 @@
 def diffList (list1, list2):
+  orgList1 = list1.copy()
+  orgList2 = list2.copy()
   for elm in list1:
     if elm in list2:
       list1.remove(elm)
@@ -9,6 +11,6 @@ def diffList (list1, list2):
       list2.remove(elm)
   
   diffrence = list1 + list2
-  print(diffrence)
+  print(f"\nThe Diffrence between List One:\n{orgList1}\n\nand List Two:\n{orgList2}\n\nwas...\n\n\n{diffrence}")
   return diffrence
 diffList(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"])
