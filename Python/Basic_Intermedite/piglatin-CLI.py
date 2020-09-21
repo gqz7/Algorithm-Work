930c76ae815dde958971c27e6db77b5548216ac1
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-20 23:43:35 -0400

    starting piglatin CLI

---
+++ Python/Basic_Intermedite/piglatin-CLI.py
@@ -0,0 +1,9 @@
+welcomeMsg = "\nWelcome to the Pig-Latin CLI\n\nYou will enter a word or phrase and your entry will be responded back in Pig-Latin\n"
+invalidMsg = "\nThat's Not A Valid Entry, Try Again.\n"
+
+def init():
+    int1 = input("Enter A Word Or Phrase\nInput: ")
+
+
+print(welcomeMsg)
+init()
