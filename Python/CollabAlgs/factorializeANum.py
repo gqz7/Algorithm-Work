fae1f01c5a4ea08611fba64f7cc168aadf16aca3
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-22 23:34:45 -0400

    algorithm worked on by J.P

---
+++ Python/CollabAlgs/factorializeANum.py
@@ -0,0 +1,17 @@
+# https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/factorialize-a-number
+
+#keep going jeremy
+#margie is messing w jeremy
+#You did it!
+
+
+def factorial ():
+    demo = int(input("enter a number to factorialize "))
+    count = 1
+    num = 1
+    while count < demo:
+        count += 1
+        num *= count
+        print(num)
+factorial()
+    
