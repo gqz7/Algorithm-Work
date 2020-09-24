65c69f8d6f16b837e0d47c2102267bb6863e3a1e
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-23 23:53:37 -0400

    complete piglatin CLI with program loop

--- Python/Basic_Intermedite/piglatin-CLI.py
+++ Python/Basic_Intermedite/piglatin-CLI.py
@@ -2,9 +2,10 @@ import re
 
 welcomeMsg = "\nWelcome to the Pig-Latin CLI\n\nYou will enter a word or phrase and your entry will be responded back in Pig-Latin\n"
 invalidMsg = "\nThat's Not A Valid Entry, Try Again.\n"
+tryAginMsg = '\nWould you like to try again? (y/n)\nInput: '
 
 def init():
-    userInput = input("Enter A Word Or Phrase\nInput: ")
+    userInput = input("\nEnter A Word Or Phrase\nInput: ")
 
     if userInput.strip() == "":
       print(invalidMsg)
@@ -18,6 +19,7 @@ def init():
       newWord = toPigLatin(word)
       newPhrase += newWord + ' ' 
     print(f'\nYour Pig Latin Phrase Translates To:\n\n{newPhrase}')
+    tryAgain()
 
 def toPigLatin(str):
   if not re.search("[a-zA-Z]", str):
@@ -28,6 +30,16 @@ def toPigLatin(str):
     vIndex = int(re.search("a|e|i|o|u?i", str).start())
     pStr = str[vIndex:] + str[0:vIndex] + 'ay'
     return pStr
+
+def tryAgain():
+  ta = input(tryAginMsg)[0].lower()
+  if ta == 'y':
+    init()
+  elif ta == 'n':
+    return
+  else: 
+    print('\nEnter yes or no')
+    tryAgain()
 
 print(welcomeMsg)
 
