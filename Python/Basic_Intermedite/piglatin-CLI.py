9663427f155ceb0f1806706953bc86b35c7a3ae2
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-23 22:20:19 -0400

    complete piglatin CLI

--- Python/Basic_Intermedite/piglatin-CLI.py
+++ Python/Basic_Intermedite/piglatin-CLI.py
@@ -1,3 +1,5 @@
+import re
+
 welcomeMsg = "\nWelcome to the Pig-Latin CLI\n\nYou will enter a word or phrase and your entry will be responded back in Pig-Latin\n"
 invalidMsg = "\nThat's Not A Valid Entry, Try Again.\n"
 
@@ -10,11 +12,23 @@ def init():
     
     stringList = userInput.split()
 
+    newPhrase = ''
+
     for word in stringList:
-      newWord = toPigLatin(word) 
+      newWord = toPigLatin(word)
+      newPhrase += newWord + ' ' 
+    print(f'\nYour Pig Latin Phrase Translates To:\n\n{newPhrase}')
 
 def toPigLatin(str):
-  print(str)
+  if not re.search("[a-zA-Z]", str):
+    return str
+  elif not re.search("a|e|i|o|u?i", str):
+    return str + 'ay'
+  else:
+    vIndex = int(re.search("a|e|i|o|u?i", str).start())
+    pStr = str[vIndex:] + str[0:vIndex] + 'ay'
+    return pStr
 
 print(welcomeMsg)
+
 init()
