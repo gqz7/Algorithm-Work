9be1dcad1fb5ae209808b6b1f90a52a2babed432
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-24 23:46:54 -0400

    implement recersive solution to nested arrays

--- Python/Basic_Intermedite/largestNumInList.py
+++ Python/Basic_Intermedite/largestNumInList.py
@@ -1,12 +1,17 @@
-def largestInList (list):
+def largestInList (nlist):
   largest = None
-  for num in list:
+  for num in nlist:
+    if isinstance(num, list):
+      num = largestInList(num)
+    
     numStr = str(num)
     if largest == None and numStr.isnumeric() :
       largest = int(num)
     if numStr.isnumeric() and int(num) > largest:
       largest = int(num)
-  print(largest)
+  if not largest == None:  
+    print(f"\n\nLargest Num In\n{nlist} is...\n{largest}")
+  
 
-
-largestInList([4, 5, 1, 3])
+# largestInList([17, 23, 25, 12])
+largestInList([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21]])
