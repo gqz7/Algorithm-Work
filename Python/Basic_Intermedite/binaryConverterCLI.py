f8b76c65c128ba77d0cf987309589cf458b3f666
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-30 13:12:31 -0400

    complete algorithmfor binary to string so this CLI is now a two-way converter

--- Python/Basic_Intermedite/binaryConverterCLI.py
+++ Python/Basic_Intermedite/binaryConverterCLI.py
@@ -5,16 +5,43 @@ invalidMsg = "\nThat's Not A Valid Entry, Try Again.\n"
 tryAginMsg = '\nWould you like to try again? (y/n)\nInput: '
 
 def init():
-    userInput = input("\nEnter A Word OR Phrase\nInput: ")
 
+    choice = input('Would you like to convert...\n0. Binary To String\n1. String To Binary\n\nNumber: ')
+
+    if not choice == '0' and not choice == '1' :
+      print('\nYou must enter 1 or 0\n')
+      init()
+
+    inputMsg = "\nEnter A Word OR Phrase\nString: " if choice == '1' else "\nEnter A Binary String (ASCII UTF-8)\nBinary: "
+    userInput = input(inputMsg) 
     if userInput.strip() == "":
       print(invalidMsg)
       init()
 
-    binary = stringToBinary(userInput) #enter user input as param
+    if choice == '1':  
+      binary = stringToBinary(userInput) #enter user input as param
+      print(f'\nYour Binary String Is...\n\n{binary}')
+    else:
+      string = biToStr(userInput) #enter user input as param
+      print(f'\nYour String Is...\n\n{string}')
 
-    print(f'\nYour Binary String Is...\n\n{binary}')
     tryAgain()
+
+def biToStr (binary): 
+
+  finalStr = ''
+  splitBi = binary.split(' ')
+  for charInBi in splitBi:  
+    base = len(charInBi)-1
+    index = 0
+    charNum = 0
+    while base >= 0:
+      if charInBi[index] == '1':
+        charNum += 2 ** base
+      index += 1
+      base -= 1
+    finalStr += chr(charNum)
+  return finalStr
 
 def stringToBinary(string):
   
@@ -24,7 +51,7 @@ def stringToBinary(string):
     utf8Num = ord(char) 
     base=8
     biStr = ''
-    while base > -1:
+    while base >= 0:
       if utf8Num - (2 ** base) >= 0:
         utf8Num-=2**base
         # print(utf8Num, base, 2**base) 
