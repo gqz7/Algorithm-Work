7416c6a830fb07af2aeb60485f5b6ebbc29a9bf1
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-09-29 23:57:24 -0400

    starting work on binary to string CLI translator

---
+++ Python/Basic_Intermedite/binaryConverterCLI.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+welcomeMsg = "\nWelcome to the Binary Converted CLI\n\nEnter a string and it will be converted to binary as UTF-8 number codes\n"
+invalidMsg = "\nThat's Not A Valid Entry, Try Again.\n"
+tryAginMsg = '\nWould you like to try again? (y/n)\nInput: '
+
+def init():
+    userInput = input("\nEnter A Word OR Phrase\nInput: ")
+
+    if userInput.strip() == "":
+      print(invalidMsg)
+      init()
+
+    binary = stringToBinary(userInput) #enter user input as param
+
+    print(f'\nYour Binary String Is...\n\n{binary}')
+    tryAgain()
+
+def stringToBinary(string):
+  
+  binary = ''
+  
+  for char in string:
+    utf8Num = ord(char) 
+    base=8
+    biStr = ''
+    while utf8Num > 1 and base > -1:
+      if utf8Num - (2 ** base) > 0:
+        utf8Num-=2**base
+        print(utf8Num, base, 2**base) 
+      # print(utf8Num, base, 2**base)
+      base-=1
+
+
+
+  return binary
+
+def tryAgain():
+  ta = input(tryAginMsg)[0].lower()
+  if ta == 'y':
+    init()
+  elif ta == 'n':
+    return
+  else: 
+    print('\nEnter yes or no')
+    tryAgain()
+
+print(welcomeMsg)
+
+init()
