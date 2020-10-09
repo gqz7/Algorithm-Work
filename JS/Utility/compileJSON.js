0906512df96a7447a5952e64b3a6013579d95c85
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-09 03:50:09 -0400

    new algorithm to compile json data from many JSONs to just one array

---
+++ JS/Utility/compileJSON.js
@@ -0,0 +1,22 @@
+//This algorithm take a directory path and an object key. 
+//The main purpose is to grab array data from many json files and compile the data into one array in a single JSON file.
+const fs = require('fs');
+
+const compileJson = ({path, fileName, key, start, limit, outFileName}) => {
+
+  const finalArr = []
+
+    for (let i = start; i <= limit; i++) {
+      
+      const filePath = `${path}${fileName}${i}.json`
+
+      const raw = JSON.parse(fs.readFileSync(filePath));
+
+      for (let i = 0; i < raw[key].length; i++) {
+        const element = JSON.parse(raw[key][i]);
+        finalArr.push( element )
+      }
+    }
+
+    console.log(finalArr.length);
+}
