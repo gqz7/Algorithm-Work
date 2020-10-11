711a66125a2d5266b5232d0f8a08ebf09a657552
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-10 23:58:28 -0400

    start java fib alg

---
+++ Java/fibonacci.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("This is the Fibonacci Sequence");
+		int i = 0;
+		int prev = 0;
+		int cur = 1;
+		int next = 0;
+		while ( i < 10 ) {
+		    String msg = "FibNum" + next;
+		    System.out.println();
+		    i++;
+		}
+	}
+}
