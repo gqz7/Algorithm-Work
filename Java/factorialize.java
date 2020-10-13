588d077e3f05657570b64b57cf6b8e92a5c34118
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-12 23:54:16 -0400

    algorithm CLI to factorialize numbers

---
+++ Java/factorialize.java
@@ -0,0 +1,26 @@
+package com.astralprojection;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+/*
+  User enters number and gets the factorial of it
+ */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter an integer to be factorialized\nInt: ");
+        int number = scanner.nextInt();
+
+        int factorial = 1, count = 1;
+
+        while (count <= number) {
+          factorial *= count;
+          count++;
+        }
+
+        System.out.println("\nThe Factorial Of " + number + ' is ' + factorial);
+
+    }
+}
