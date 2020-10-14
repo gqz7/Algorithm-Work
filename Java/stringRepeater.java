8a951fe83bc2aa8f62ccdb00c1e21aae1d40f867
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-13 23:11:24 -0400

    create a algorithm to repeat a string with inputs for the string and number of repeats

---
+++ Java/stringRepeater.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    /*
+      User enters number and gets the factorial of it
+     */
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the string repeater CLI!\nYou will first enter a string, then the number of times you want the string to repeat\nThen the repeated string will output\n\n");
+        System.out.print("Enter an string to be repeated\nString: ");
+        String orgStr = scanner.nextLine();
+        String repeatedStr = "";
+
+        System.out.print("Enter how many times you want the string to be repeated\nInt: ");
+        int repeatTimes = scanner.nextInt();
+
+        for ( int i = 0; i < repeatTimes; i++) {
+
+            repeatedStr += orgStr;
+        }
+
+        System.out.println("\nYour Repeated String Is: \n\n" + repeatedStr);
+
+    }
+}
