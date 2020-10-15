6bf8442042fb9d3fe45f81a9680a158071b38870
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-14 23:29:42 -0400

    implement user input in string reverser algorithm and add exception handling

--- Java/stringReverser.java
+++ Java/stringReverser.java
@@ -2,20 +2,26 @@ import java.text.*;
 public class Main
 {
 	public static void main(String[] args) {
-		
-		String testString = "This is a test string!";
-		
-		String reversedString = "";
-		   
-		int strLen = testString.length()-1;
-		
-		for (int i = strLen; i >= 0 ; i--) {
-		  //  System.out.println(testString.charAt(i));
-		    reversedString += testString.charAt(i);
+		try {
+			final Scanner scanner = new Scanner(System.in);
+
+			String normalString = scanner.nextLine();
+			
+			String reversedString = "";
+				
+			int strLen = normalString.length()-1;
+			
+			for (int i = strLen; i >= 0 ; i--) {
+				//  System.out.println(normalString.charAt(i));
+					reversedString += normalString.charAt(i);
+			}
+			
+			String finalMsg = MessageFormat.format("\nYour Original String:\n{0}\n\nYour String Reversed:\n{1}", normalString, reversedString);
+			
+			System.out.println(finalMsg);
+
+		} catch (Exception e) {
+				System.out.println("You must enter a String for this program");
 		}
-		
-		String finalMsg = MessageFormat.format("\nYour Original String:\n{0}\n\nYour String Reversed:\n{1}", testString, reversedString);
-		
-		System.out.println(finalMsg);
 	}
 }
