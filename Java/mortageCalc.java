6a81d57efcb4b992b7ab47d821275b1af4e1fa22
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-15 23:08:45 -0400

    clean up code in mortgage claculator

--- Java/mortageCalc.java
+++ Java/mortageCalc.java
@@ -10,12 +10,21 @@ public class Main {
     Period (Years taken as months): how long to pay off loan
      */
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        final int MONTHS_IN_YEAR = 12, PERCENT = 100;
-
         try {
+
+            //CONTANTS
+            final int MONTHS_IN_YEAR = 12, PERCENT = 100;
+            final Scanner scanner = new Scanner(System.in);
+            
+            //VALUES THAT WILL BE SET BY USER INPUT
             //Principal is the total loan in $USD
-            float principal = 0;
+                float principal = 0;
+            //Number of months the loan is being taken out
+                float months  = 0;
+            //Percentage is the interest rate on each 12 month period
+                float percentage = 0;
+
+            //GET INPUT FOR PRINCIPAL/LOAN AMMOUNT
             while (principal < 500 || principal >10000000) {
 
                 System.out.print("How much do you want to take out on your loan? ($500 - 10,000,000)\nUSD: $");
@@ -24,19 +33,19 @@ public class Main {
                      System.out.print("\nEnter a number in the range of 500 to 10,000,000\n");
                 }
             }
-            float percentage = 0;
+
+            //GET INPUT FOR PERCENTAGE/INTEREST RATE
             while (percentage < 1 || percentage > 100) {
                 System.out.print("\nWhat is the yearly interest rate on your loan? (1 - 100)\nPercent: ");
-                //Percentage is the interest rate on each 12 month period
                 percentage = scanner.nextFloat();
                 if (principal < 1 || principal > 100) {
                      System.out.print("\nEnter a number in the range of 1 to 100\n");
                 }
             }
-            float months  = 0;
+
+            //GET INPUT FOR MONTHS
             while (months < 1 || months > 700) {
                 System.out.print("\nHow many months until you repay the loan? (1-700)\nMonths: ");
-                //Period will be the number of months the loan is being taken out
                 months = scanner.nextFloat();
                 if (principal < 1 || principal > 700) {
                      System.out.print("\nEnter a number in the range of 1 to 700\n");
@@ -44,6 +53,7 @@ public class Main {
 
             }
 
+            //CALCULATE MORTGAGE
             float numberOfPayments = months*MONTHS_IN_YEAR;
             float rate = percentage/PERCENT/MONTHS_IN_YEAR;
 
