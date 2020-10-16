9b7d60bd00fa37283c72e232a526b411bd589223
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-16 19:39:56 -0400

    signifigantly clean code for mortgage calculator, now accounts for non number inputs and does not have to exit process and acceptable ranges are accounted for

--- Java/mortageCalc.java
+++ Java/mortageCalc.java
@@ -9,62 +9,59 @@ public class Main {
     Annual Interest: percentage each year
     Period (Years taken as months): how long to pay off loan
      */
+
     public static void main(String[] args) {
-        try {
-
-            //CONTANTS
-            final int MONTHS_IN_YEAR = 12, PERCENT = 100;
-            final Scanner scanner = new Scanner(System.in);
-            
-            //VALUES THAT WILL BE SET BY USER INPUT
-            //Principal is the total loan in $USD
-                float principal = 0;
-            //Number of months the loan is being taken out
-                float months  = 0;
-            //Percentage is the interest rate on each 12 month period
-                float percentage = 0;
-
-            //GET INPUT FOR PRINCIPAL/LOAN AMMOUNT
-            while (principal < 500 || principal >10000000) {
-
-                System.out.print("How much do you want to take out on your loan? ($500 - 10,000,000)\nUSD: $");
-                principal = scanner.nextFloat();
-                if (principal < 500 || principal >10000000) {
-                     System.out.print("\nEnter a number in the range of 500 to 10,000,000\n");
-                }
-            }
-
-            //GET INPUT FOR PERCENTAGE/INTEREST RATE
-            while (percentage < 1 || percentage > 100) {
-                System.out.print("\nWhat is the yearly interest rate on your loan? (1 - 100)\nPercent: ");
-                percentage = scanner.nextFloat();
-                if (principal < 1 || principal > 100) {
-                     System.out.print("\nEnter a number in the range of 1 to 100\n");
-                }
-            }
-
-            //GET INPUT FOR MONTHS
-            while (months < 1 || months > 700) {
-                System.out.print("\nHow many months until you repay the loan? (1-700)\nMonths: ");
-                months = scanner.nextFloat();
-                if (principal < 1 || principal > 700) {
-                     System.out.print("\nEnter a number in the range of 1 to 700\n");
-                }
-
-            }
-
-            //CALCULATE MORTGAGE
-            float numberOfPayments = months*MONTHS_IN_YEAR;
-            float rate = percentage/PERCENT/MONTHS_IN_YEAR;
-
-            float mortgage = (float) (principal * rate * Math.pow(1+rate, numberOfPayments)/(Math.pow(1+rate, numberOfPayments)-1) );
-
-            String finalMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
-
-            System.out.println("\nYour annual mortgage will be: " + finalMortgage);
-        } catch (Exception e) {
-            System.out.println("You must correct data types");
-        }
-
+        collectData();
     }
+
+    private static void collectData() {
+
+        //GET INPUT FOR PRINCIPAL/LOAN AMMOUNT
+        String principleQues = "How much do you want to take out on your loan? ($500 - 10,000,000)\nUSD: $";
+        float princeAmount = askQuestion( principleQues, 500, 10000000);
+
+        //GET INPUT FOR PERCENTAGE/INTEREST RATE
+        String interestQues = "\nWhat is the yearly interest rate on your loan? (1 - 100)\nPercent: ";
+        float interestAmount = askQuestion( interestQues, 1, 100);
+
+        //GET INPUT FOR MONTHS
+        String monthsQues = "\nHow many months until you repay the loan? (1-700)\nMonths: ";
+        float monthsAmount = askQuestion( monthsQues, 1, 700);
+
+        calculateMortgage(princeAmount, monthsAmount, interestAmount);
+    };
+
+    private static void calculateMortgage(float principal, float months, float percentage) {
+        //CONTANTS
+        final int MONTHS_IN_YEAR = 12, PERCENT = 100;
+
+        //CALCULATE MORTGAGE
+        float numberOfPayments = months*MONTHS_IN_YEAR;
+        float rate = percentage/PERCENT/MONTHS_IN_YEAR;
+
+        float mortgage = (float) (principal * rate * Math.pow(1+rate, numberOfPayments)/(Math.pow(1+rate, numberOfPayments)-1) );
+
+        String finalMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
+
+        System.out.println("\nYour annual mortgage will be: " + finalMortgage);
+    };
+
+    private static float askQuestion(String question, int min, int max) {
+        float inputValue = 0;
+        final Scanner scanner = new Scanner(System.in);
+
+        try {
+            while (inputValue < min || inputValue > max) {
+                System.out.print(question);
+                inputValue = scanner.nextFloat();
+                if (inputValue < min || inputValue > max) {
+                    System.out.print("\nWARNING\nYou did not enter a number in the range of "+min+" to "+max+"\n\n");
+                }
+            }
+            return inputValue;
+        } catch (Exception e) {
+            System.out.println("\nYou must correct data types");
+            return askQuestion(question, min, max);
+        }
+    };
 }
