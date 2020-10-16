df7285597609d7eb2b962bd0b771a4d8ee46957c
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-15 23:03:40 -0400

    add in logs to remind user of ranges required values

--- Java/mortageCalc.java
+++ Java/mortageCalc.java
@@ -20,18 +20,28 @@ public class Main {
 
                 System.out.print("How much do you want to take out on your loan? ($500 - 10,000,000)\nUSD: $");
                 principal = scanner.nextFloat();
+                if (principal < 500 || principal >10000000) {
+                     System.out.print("\nEnter a number in the range of 500 to 10,000,000\n");
+                }
             }
             float percentage = 0;
-            while (percentage < 30 || percentage > 100) {
-                System.out.print("\nWhat is the yearly interest rate on your loan? (30 - 100)\nPercent: ");
+            while (percentage < 1 || percentage > 100) {
+                System.out.print("\nWhat is the yearly interest rate on your loan? (1 - 100)\nPercent: ");
                 //Percentage is the interest rate on each 12 month period
                 percentage = scanner.nextFloat();
+                if (principal < 1 || principal > 100) {
+                     System.out.print("\nEnter a number in the range of 1 to 100\n");
+                }
             }
             float months  = 0;
             while (months < 1 || months > 700) {
                 System.out.print("\nHow many months until you repay the loan? (1-700)\nMonths: ");
                 //Period will be the number of months the loan is being taken out
                 months = scanner.nextFloat();
+                if (principal < 1 || principal > 700) {
+                     System.out.print("\nEnter a number in the range of 1 to 700\n");
+                }
+
             }
 
             float numberOfPayments = months*MONTHS_IN_YEAR;
