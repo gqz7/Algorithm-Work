772b012730676b2a55094893f4d4e62b72c03bb5
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-15 23:55:24 -0400

    allow for user input on algorithm

--- Java/sumOfNumsInRange.java
+++ Java/sumOfNumsInRange.java
@@ -1,19 +1,35 @@
 package com.astralprojection;
 
+import java.util.Scanner;
+
 public class Main {
-/*
-  User enters two number and gets the range of numbers between them
- */
+    /*
+      User enters two number and gets the range of numbers between them
+     */
     public static void main(String[] args) {
-      int min = 1;
-      int max = 10;
 
-      int total = 0;
+        final Scanner scanner = new Scanner(System.in);
 
-      for ( int i = min: i <=  max; i++ ) {
-        total += i;
-      }        
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
 
-      
+        int min = 0;
+        int max = 0;
+        if ( number1 > number2 ) {
+          min = number2;
+          max = number1;
+        } else {
+          min = number1;
+          max = number2;
+        }
+
+        int total = 0;
+
+        for ( int i = min; i <=  max; i++) {
+            total += i;
+        }
+
+        System.out.println("The total sum of all numbers between " + min + " and " + max + " is " + total);
+
     }
 }
