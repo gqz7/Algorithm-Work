781909a6d0fd2077a1edbfc1c3cb84f5e9980630
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-15 23:57:01 -0400

    add exception handling in sum of range algorithm

--- Java/sumOfNumsInRange.java
+++ Java/sumOfNumsInRange.java
@@ -7,29 +7,31 @@ public class Main {
       User enters two number and gets the range of numbers between them
      */
     public static void main(String[] args) {
+      try {
+          final Scanner scanner = new Scanner(System.in);
 
-        final Scanner scanner = new Scanner(System.in);
+          int number1 = scanner.nextInt();
+          int number2 = scanner.nextInt();
 
-        int number1 = scanner.nextInt();
-        int number2 = scanner.nextInt();
+          int min = 0;
+          int max = 0;
+          if ( number1 > number2 ) {
+            min = number2;
+            max = number1;
+          } else {
+            min = number1;
+            max = number2;
+          }
 
-        int min = 0;
-        int max = 0;
-        if ( number1 > number2 ) {
-          min = number2;
-          max = number1;
-        } else {
-          min = number1;
-          max = number2;
-        }
+          int total = 0;
 
-        int total = 0;
+          for ( int i = min; i <=  max; i++) {
+              total += i;
+          }
 
-        for ( int i = min; i <=  max; i++) {
-            total += i;
-        }
-
-        System.out.println("The total sum of all numbers between " + min + " and " + max + " is " + total);
-
+          System.out.println("The total sum of all numbers between " + min + " and " + max + " is " + total);
+      } catch (Exception e) {
+          System.out.println("You must enter an Integer for this program");
+      }
     }
 }
