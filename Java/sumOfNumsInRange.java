f734e67ffba52fd25b47e8ba5e53a5be3a8c6fab
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-15 23:51:52 -0400

    create simple sum of number alg

---
+++ Java/sumOfNumsInRange.java
@@ -0,0 +1,19 @@
+package com.astralprojection;
+
+public class Main {
+/*
+  User enters two number and gets the range of numbers between them
+ */
+    public static void main(String[] args) {
+      int min = 1;
+      int max = 10;
+
+      int total = 0;
+
+      for ( int i = min: i <=  max; i++ ) {
+        total += i;
+      }        
+
+      
+    }
+}
