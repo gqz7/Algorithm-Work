083db0d8b7bdc924dc40c3a23394a109cef02940
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-16 20:27:21 -0400

    implement clean code practices into title caser algorithm

--- Java/titleCaser.java
+++ Java/titleCaser.java
@@ -1,6 +1,5 @@
 package com.astralprojection;
 
-import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -8,31 +7,49 @@ public class Main {
       User enters number and gets the factorial of it
      */
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter an sentence to be Title Cased\nString: ");
 
         try {
-            String sentence = scanner.nextLine();
-            String titleCased = "";
-
-            String sentenceArr[]= sentence.split(" ");
-
-            for ( int i = 0; i < sentenceArr.length; i++) {
-                String word = sentenceArr[i];
-                String titleCasedWord = Character.toUpperCase( word.charAt(0) ) + word.substring(1).toLowerCase();
-                // System.out.println(titleCasedWord);
-                if (i != 0) {
-                    titleCased += " ";
-                }
-                titleCased += titleCasedWord;
-
-            }
-
+            String titleCaseQuestion = "Enter an sentence to be Title Cased\nString: ";
+            String originalStr = takeUserString(titleCaseQuestion);
+            String titleCased = titleCaseString(originalStr);
             System.out.println("\nYour Title Cased Sentence Is: \n" + titleCased);
         }catch (Exception e) {
-            System.out.println("You must enter an String for this program");
+            System.out.println("An unexpected error occured!");
         }
 
     }
+
+    public static String titleCaseString (String inStr ) {
+        String titleCased = "";
+        String originalStr[]= inStr.split(" ");
+
+        for ( int i = 0; i < originalStr.length; i++) {
+            String word = originalStr[i];
+            String titleCasedWord = Character.toUpperCase( word.charAt(0) ) + word.substring(1).toLowerCase();
+            // System.out.println(titleCasedWord);
+            if (i != 0) {
+                titleCased += " ";
+            }
+            titleCased += titleCasedWord;
+        }
+        return titleCased;
+    }
+
+    public static String takeUserString (String question) {
+        final Scanner scanner = new Scanner(System.in);
+
+        String inputStr;
+
+        while (true) {
+            System.out.print(question);
+
+            inputStr = scanner.nextLine();
+            if (!inputStr.strip().equals("")) {
+                break;
+            }
+            System.out.println("\nWARNING: You entered nothing.\n");
+        }
+        return inputStr;
+    }
+
 }
