3aff6e9ffd4460b9c67a74ca0722642fbee867c4
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-19 22:47:05 -0400

    now utilizes accountHoder field which has added a param to the account class contructor, yearly mortgage was mislabled as yearly where it should be monthly. This now fixes small bug which would display the wrong ammoutn for the monthly payments

--- Java/OOP-Basics/MortgageCalulator/MortgageAccount.java
+++ Java/OOP-Basics/MortgageCalulator/MortgageAccount.java
@@ -6,26 +6,29 @@ import java.text.NumberFormat;
 public class MortgageAccount {
     final static byte MONTHS_IN_YEAR = 12, PERCENT = 100;
 
+    private String accountHolder;
     private float principle;
     private float yearlyInterest;
     private float monthlyInterest;
-    public float yearlyMortgage;
+    public float monthlyMortgage;
     private byte yearPeriod;
     private short monthPeriod;
 
     MortgageAccount(
+            String name,
             float total,
             float interest,
             byte period
     ) {
-       this.principle = total;
-       this.yearlyInterest = interest;
-       this.yearPeriod = period;
+       accountHolder = name;
+       principle = total;
+       yearlyInterest = interest;
+       yearPeriod = period;
 
-       this.monthlyInterest = yearlyInterest / PERCENT / MONTHS_IN_YEAR;
-       this.monthPeriod = (short) (yearPeriod * MONTHS_IN_YEAR);
+       monthlyInterest = yearlyInterest / PERCENT / MONTHS_IN_YEAR;
+       monthPeriod = (short) (yearPeriod * MONTHS_IN_YEAR);
 
-       this.yearlyMortgage = calculateMortgage(principle, yearPeriod, yearlyInterest);
+       monthlyMortgage = calculateMortgage(principle, yearPeriod, yearlyInterest);
     }
 
     private float calculateMortgage(float principal, float months, float percentage) {
@@ -45,8 +48,15 @@ public class MortgageAccount {
     public void displayPaymentSchedule() {
 
         String lineSeparator = "_______________________________________________________________";
-        String monthlyPayFormatted = NumberFormat.getCurrencyInstance().format(this.yearlyMortgage/MONTHS_IN_YEAR);
-        System.out.println(MessageFormat.format("\nYour loan of ${0} will be paid off in {1} monthly payments of {2}\n{3}\n\n", this.principle, this.monthPeriod, monthlyPayFormatted, lineSeparator));
+        String monthlyPayFormatted = NumberFormat.getCurrencyInstance().format(this.monthlyMortgage);
+        System.out.println(MessageFormat.format(
+                "\nThank you for using our service {4}.\nYour loan of ${0} will be paid off in {1} monthly payments of roughly {2}\n{3}\n\n",
+                this.principle,
+                this.monthPeriod,
+                monthlyPayFormatted,
+                lineSeparator,
+                accountHolder
+        ));
 
         int currentMonth = 0;
         while (currentMonth <= this.monthPeriod) {
