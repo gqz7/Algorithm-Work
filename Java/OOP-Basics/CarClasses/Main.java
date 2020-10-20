0fea6f97d51cb8c77e28e05f1bf03a1204ca508f
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-19 22:25:37 -0400

    test out toString override

--- Java/OOP-Basics/CarClasses/Main.java
+++ Java/OOP-Basics/CarClasses/Main.java
@@ -4,16 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        var regularCar = new Car();
-        regularCar.moveForward(10);
-        regularCar.toggleMovingForward();
-        regularCar.moveForward(2);
-
-        System.out.println(regularCar.xPosition);
+        var regularCar = new Car(100, "pink", "billy");
+        regularCar.moveForward(27);
+//        regularCar.toggleMovingForward();
+//        regularCar.moveForward(2);
+//        System.out.println(regularCar.xPosition);
 
         var myTruck = new Truck( 2000 );
-        myTruck.setCurrentLoad( 1000 );
+        myTruck.moveForward(77);
+//        myTruck.setCurrentLoad( 1000 );
+//        System.out.println(myTruck.getCurrentLoad());
 
-        System.out.println(myTruck.getCurrentLoad());
+        System.out.println(myTruck.toString());
+        System.out.println(regularCar.toString());
     }
 }
