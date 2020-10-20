7412de9ef9899eb596a2e4b4a1ea56cdb6be0c3b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-19 22:04:32 -0400

    child class for Car

---
+++ Java/OOP-Basics/CarClasses/Truck.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Truck extends Car {
+
+    private int maxLoad;
+    private float currentLoad;
+
+    Truck ( int loadWeight ) {
+        maxLoad = loadWeight;
+        currentLoad = 0;
+    }
+
+    public float getCurrentLoad () {
+        return currentLoad;
+    }
+
+    public void setCurrentLoad ( int weight ) {
+        if ( weight <= maxLoad ) {
+            currentLoad = weight;
+        } else {
+            System.out.println("The load weight is too much for your car");
+        }
+    }
+}
