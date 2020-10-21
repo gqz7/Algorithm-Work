a289e431c90329809f079fc1ec1df39e39d47bba
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-21 12:37:55 -0400

    running test to learn about overriding, polymorphism, inheritance and up/down-casting objects

---
+++ Java/OOP-Basics/UserInterface/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TextBox inputName = new TextBox();
+//        System.out.println(inputName.isEnabled());
+
+        UIControl testControl = new UIControl(false);
+
+        inputName.setText("Billy Jean"); //sets the innerText field of the text box
+//        System.out.println(inputName); //because the toString method has been overridden this should return the innerText field of the TextBox
+
+//        castingObj(inputName);
+//        castingObj(testControl);
+
+        //example polymorphism
+        UIControl[] allControls = { inputName, new CheckBox()};
+
+        for (var control : allControls)
+            control.render();
+    }
+
+    public static void castingObj ( UIControl obj) {
+        if (obj instanceof TextBox) {
+            ((TextBox) obj).setText("I was casted"); //this is an example of down-casting because the parent class is now casted to a child class
+        }
+        System.out.println(obj);
+
+    }
+}
