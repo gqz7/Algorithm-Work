6f35e42d4b3abc0e97f63b254c456abc8e93a9c2
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-20 22:33:41 -0400

    implement withdraw logic with monthly limits

--- Java/OOP-Basics/Bank/SavingsAccount.java
+++ Java/OOP-Basics/Bank/SavingsAccount.java
@@ -3,10 +3,31 @@ package com.company;
 public class SavingsAccount extends BankAccount {
 
     private float interest = (float) 0.01;
+    private static final int WITHDRAW_FEE = 10_000;
+    private static final int WD_LIMIT = 6;
+    private int monthlyWithdraws = 0;
 
     SavingsAccount( String firstName, String lastName) {
-
         super(firstName, lastName);
+    }
+    private void monthlyInterestGain () {
 
     }
+
+    @Override
+    public void withdraw ( int amount ) {
+        int totalWithdraw;
+        if (monthlyWithdraws+1 > WD_LIMIT) {
+            totalWithdraw = amount + WITHDRAW_FEE;
+            System.out.println("Over monthly withdraw limit, you will be charged a withdraw fee.");
+        } else totalWithdraw = amount;
+
+        super.withdraw(totalWithdraw);
+        monthlyWithdraws += 1;
+    }
+    @Override
+    public void deposit ( int amount ) {
+        super.deposit(amount);
+    }
+
 }
