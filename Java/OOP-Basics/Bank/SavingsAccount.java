6a9996cf89ff5413a5ee209c2b8e9b5db9df42d9
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-20 20:57:42 -0400

    inital commit for saving account class

---
+++ Java/OOP-Basics/Bank/SavingsAccount.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class SavingsAccount extends BankAccount {
+
+    private float interest = (float) 0.01;
+
+    SavingsAccount( String firstName, String lastName) {
+
+        super(firstName, lastName);
+
+    }
+}
