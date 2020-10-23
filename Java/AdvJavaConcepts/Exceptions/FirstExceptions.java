ef4191affb892fed5e57ce770ce16ad415673d80
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-23 14:57:51 -0400

    demo a handled exception with the file-reader class

--- Java/AdvJavaConcepts/Exceptions/FirstExceptions.java
+++ Java/AdvJavaConcepts/Exceptions/FirstExceptions.java
@@ -1,13 +1,20 @@
 package com.company;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class FirstExceptions {
 
-    public void showWithNoHandler() {
+    public void errorWithNoHandler() {
         sayHello(null); //this will throw a NullPointerException and log the call stack
     }
 
-    public void showWithHandler() {
-
+    public void errorWithHandler(String fileName) {
+        try {
+            FileReader reader = new FileReader(fileName);
+        } catch (FileNotFoundException exception) {
+            System.out.println("File does not exist");
+        }
     }
 
     public static void sayHello( String name ) {
