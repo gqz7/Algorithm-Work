026d2d2356e462adf655d1de77963d378decec57
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-22 23:55:27 -0400

    clean up code, working on encapulating all functionality

--- Java/CLI-package/Factorial.java
+++ Java/CLI-package/Factorial.java
@@ -3,23 +3,22 @@ import java.text.MessageFormat;
 
 public class Factorial extends Algorithm {
 
+    String question = "Enter an integer to factorialize (0 - 20)\nInt: ";
+
     @Override
     public void run () {
-        String factorialQuestion = "Enter an integer to factorialize (0 - 20)\nInt: ";
-        long number = CLI.numberLongQuestion(factorialQuestion, 0, 20);
-        long factorial = calcFactorial(number);
+        long number = CLI.numberLongQuestion( question, 0, 20);
+        long factorial = calculate(number);
         System.out.println(MessageFormat.format( "\nThe Factorial Of {0} is {1}", number, factorial ));
     }
 
-    private static long calcFactorial(long num) {
+    private long calculate(long num) {
         long factorial = 1, count = 1;
         while (count <= num) {
             factorial *= count;
-//            System.out.println( MessageFormat.format ("{0} count {0}", factorial,  count);
             count++;
         }
         return factorial;
     }
-
 
 }
