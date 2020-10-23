809f00e3e078dfa5d5652632ef701f1d4a811e47
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-23 14:59:40 -0400

    add notes on common exceptions and run the handled exception error method

--- Java/AdvJavaConcepts/Exceptions/Main.java
+++ Java/AdvJavaConcepts/Exceptions/Main.java
@@ -1,11 +1,33 @@
 package com.company;
 
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
 
         var exceptionThrower = new FirstExceptions();
 
-        exceptionThrower.showWithNoHandler();
+        //exceptionThrower.errorWithNoHandler(); //this method purposefully throws an unhandled NullPointerException error
+
+        //this would cause a 'checked error' meaning the compiler will not run until the error has been fixed.
+        //  FileReader reader = new FileReader("notAFile.txt");
+        //      see caught example in FirstExceptions Class
+
+        //this will log a helpful error message to the client
+        exceptionThrower.errorWithHandler("aRandomFile.txt");
+
+        /*
+            Common Runtime Exceptions
+
+            NullPointerException - null was passed where a specific value is needed
+            ArithmeticException - trying to divide a number by 0
+            IllegalArgumentException - a method received and unexpected data type
+            IndexOutOfBoundsException - accessing an array/list index that does not exist
+            IllegalStateException - running a method but the underlying object is not in the right state
+
+
+        */
+
     }
 }
