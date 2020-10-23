c92aa2e016ce92743ee25aeb04ec510b2d0a7a5a
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-22 23:21:45 -0400

    starts factorialize algorithm

---
+++ Java/CLI-package/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.Scanner;
+import java.text.MessageFormat;
+
+public class Main {
+    /*
+      User enters number and gets the factorial of it
+     */
+    public static void main(String[] args) {
+        new Factorial().run();
+    };
+}
+
