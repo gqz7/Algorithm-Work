f6dccba3150c0116f72b28319af5223a4724ca5a
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-25 09:56:10 -0400

    add withdraw method to implment new custom exception class

--- Java/AdvJavaConcepts/Exceptions/Account.java
+++ Java/AdvJavaConcepts/Exceptions/Account.java
@@ -2,11 +2,22 @@ package com.company;
 
 import java.io.IOException;
 
+
 public class Account {
+
+    private int balance;
+
     public void deposit ( int value) throws IOException {
         if ( value <= 0 )
             throw new IOException(); //this technique of throwing exceptions is call defensive coding
 
         System.out.println(value + " is a valid number");
+    }
+
+    public void withdraw ( int value) throws InsufficientFundsException {
+        if (value > balance) {
+            throw new InsufficientFundsException();
+        }
+
     }
 }
