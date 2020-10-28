31d1a037a3ad6d26c668f16642ed0d52bbfa7f16
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-27 23:31:10 -0400

    add in passengers and operators and max passengers for vehicles, create controlled setters for thoes fields

--- Java/OOP-Basics/Vehicles/Vehicle.java
+++ Java/OOP-Basics/Vehicles/Vehicle.java
@@ -4,13 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Vehicle extends Entity {
-    public List<Passenger> passengers;
-    public Operator operator;
+    private List<Passenger> passengers;
+    private Operator operator = null;
+    private int maxPassengers;
 
-    public Vehicle(String name) {
+    public Vehicle(String name, int maxPassengers) {
         super(name);
+        this.maxPassengers = maxPassengers;
         passengers = new ArrayList<>();
-//      operator = new Operator(); //does not need to be assigned in constructor
     }
 
+    public void addOperator(Occupant occupant) {
+        if ( operator != null ) {
+            System.out.println("The operator of this vehicle can not be replaced, they need to leave the vehicle first");
+        } else {
+          operator = (Operator) occupant;
+        }
+    }
+
+    public void addPassenger(Occupant occupant) {
+        if ( passengers.size() < maxPassengers ) {
+            passengers.add( (Passenger) occupant );
+        } else {
+            System.out.println("You can not add anymore passengers to this vehicle.");
+        }
+    }
 }
