0756afe2494dd80903c554eca8b630bf66898037
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-27 22:26:44 -0400

    adding in command line interactions

---
+++ Java/OOP-Basics/Vehicles/EntityManager.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class EntityManager {
+    private static ArrayList allEntities;
+    private static ArrayList  allVehicles;
+
+    public EntityManager() {
+        System.out.println("\nSimulation Start...\n");
+    }
+}
--- Java/OOP-Basics/Vehicles/Person.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public abstract class Person extends Entity {
-    Person ( String name ) {
-        super(name);
-    }
-}
