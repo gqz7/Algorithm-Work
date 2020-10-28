f2af92d461052736324a26e76be20e013bfd0102
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-27 23:38:49 -0400

    create a method for setting an occupant as a operator

--- Java/OOP-Basics/Vehicles/Occupant.java
+++ Java/OOP-Basics/Vehicles/Occupant.java
@@ -1,7 +1,21 @@
 package com.company;
 
 public abstract class Occupant extends Entity {
+
+    private Vehicle occupying;
+
     Occupant(String name ) {
         super(name);
+    }
+
+    public void occupyVehicle ( Vehicle targetVehicle ) {
+        occupying = targetVehicle;
+        if ( this instanceof Operator ) {
+            targetVehicle.addOperator( this );
+        } else if ( this instanceof Passenger ) {
+            targetVehicle.addPassenger( this);
+        } else {
+            System.out.println("Something went wrong, Occupant needs to be Passenger or Operator");
+        }
     }
 }
