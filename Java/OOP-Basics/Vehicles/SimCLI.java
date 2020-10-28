261d4329ea0a889c3a7adad8f407977eef1e3906
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-28 03:49:47 -0400

    build basics of command line interface for simulation and vehicle creation

--- Java/OOP-Basics/Vehicles/SimCLI.java
+++ Java/OOP-Basics/Vehicles/SimCLI.java
@@ -2,8 +2,38 @@ package com.company;
 
 public class SimCLI {
 
-    public static void startSimulation() {
-        var Simulation = new EntityManager();
+    private static EntityManager Simulation;
 
+    public static void startSimulation() {
+        Simulation = new EntityManager();
+        mainMenu();
+    }
+
+    public static void mainMenu() {
+        String question = "Would you like to...\n1) Create A New Vehicle\n2) Exit the Program\nInput: ";
+        int selection = CLI.numberIntQuestion(question, 1, 2);
+
+        switch (selection) {
+            case 1:
+                SimCLI.vehicleInitialization();
+                break;
+            case 2:
+                System.out.println("\nThank you for using this program, now exiting...\n");
+                return;
+            default:
+                System.out.println("Something went wrong, input out of bounds");
+                mainMenu();
+                return;
+        }
+
+    }
+
+    private static void vehicleInitialization() {
+        var newVehicle = VehicleCLI.createVehicle();
+        if (newVehicle != null) {
+            System.out.println("Adding Vehicle To Simulation");
+        } else {
+            System.out.println("Canceled Vehicle Creation");
+        }
     }
 }
--- Java/OOP-Basics/Vehicles/VehicleCLI.java
+++ Java/OOP-Basics/Vehicles/VehicleCLI.java
@@ -1,4 +1,36 @@
 package com.company;
 
 public class VehicleCLI {
+
+    static public Vehicle createVehicle() {
+        String question = "\nWould you like to create a...\n1) Land Vehicle\n2) Air Vehicle\n3) Water Vehicle\n4) Cancel Creation Process\nInput: ";
+        int type = CLI.numberByteQuestion(question, 1, 4);
+
+        switch (type) {
+            case 1:
+                return createLandVehicle();
+            case 2:
+                return createWaterVehicle();
+            case 3:
+                return createAirVehicle();
+            case 4:
+                return null;
+            default:
+                System.out.println("Something went wrong, vehicle type out of range.");
+                return null;
+        }
+    }
+
+    static private Landcraft createLandVehicle() {
+        return new Sedan("Honda");
+    }
+
+    static private Aircraft createAirVehicle() {
+        return new PassengerPlane("747", 300);
+    }
+
+    static private Watercraft createWaterVehicle() {
+        return new MotorBoat("BoatyMcBoatFace", 20, 550);
+    }
+
 }
