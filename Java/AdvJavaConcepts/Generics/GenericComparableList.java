b9b14a227abfb6b5d745bb837213f4c752269742
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-29 15:48:00 -0400

    create a list that will allow generic classes that implement the comparable interface

---
+++ Java/AdvJavaConcepts/Generics/GenericComparableList.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class GenericComparableList<E extends Comparable> {
+
+    private E[] items;
+    private int count = 0;
+
+    public GenericComparableList(int length ) {
+        items = (E[]) new Object[length];
+    }
+
+    public void add( E item) {
+        if (count < items.length) {
+            items[count++] = item;
+        } else {
+            System.out.println("List full, can not add anymore elements");
+        }
+    }
+
+    public E get ( int index ) {
+        if ( index >= 0 && index < items.length ) {
+            return items[index];
+        } else {
+            System.out.println("Not a valid index, returning null");
+            return null;
+        }
+    }
+
+}
--- Java/AdvJavaConcepts/Generics/Main.java
+++ Java/AdvJavaConcepts/Generics/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         //any data type can be used for this list because there are no constraints
-        var intArray = new GenericList<Integer>(12); 
+        var intArray = new GenericList<Integer>(12);
 
         //Java is using boxing here because the add method takes an Integer instance but allows for an int primitive
         intArray.add(39);
@@ -14,10 +14,23 @@ public class Main {
         int firstElm = intArray.get(0);
 
         //this is fine because the constraint on this list is that is must be of a Number subclass
-        var floatArr = new GenericListWithConstraints<Float>(3); 
+        var floatArr = new GenericNumberList<Float>(13);
 
         //this will return a compile time error because String does not fit within the generics constraints
         //var strArr = new GenericListWithConstraints<String>(7);
+
+        //using the comparable interface
+        var userOne = new User(3);
+        var userTwo = new User(7);
+
+        var pointCompare = userOne.compareTo(userTwo);
+        if (pointCompare > 1) {
+            System.out.println("userOne has more points");
+        } else if ( pointCompare == 0) {
+            System.out.println("Users have the same points.");
+        } else {
+            System.out.println("userOne has less points");
+        }
 
     }
 }
