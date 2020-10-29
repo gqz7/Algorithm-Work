6505add3d9f92af466df6390e1284fa2d1593869
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-29 16:27:38 -0400

    add username and toString method to User class and create subclass of User, Admin

---
+++ Java/AdvJavaConcepts/Generics/Admin.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Admin extends User {
+    private String adminID;
+    private static int adminsMade;
+
+    public Admin () {
+        super(1000);
+        adminID = "ID-" + ++adminsMade;
+    }
+}
--- Java/AdvJavaConcepts/Generics/User.java
+++ Java/AdvJavaConcepts/Generics/User.java
@@ -2,11 +2,20 @@ package com.company;
 
 public class User implements Comparable<User> {
     public int points;
+    public String username;
+    private static int numberOfUsers;
+
     public User (int points) {
+        this.username = "User" + ++numberOfUsers;
         this.points = points;
     }
     @Override
     public int compareTo (User otherUser) {
         return points - otherUser.points;
+    }
+
+    @Override
+    public String toString () {
+        return username + ": " + points;
     }
 }
