03f98de6602195cc7b70781565a966b050946a9a
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-10-28 23:56:16 -0400

    create display methods to show all day about the entities in the simulation

--- Java/OOP-Basics/Vehicles/EntityManager.java
+++ Java/OOP-Basics/Vehicles/EntityManager.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,4 +44,46 @@ public class EntityManager {
         allPassengers.add( newEntity );
         addOccupant( newEntity );
     }
+
+    public void showAllVehicleInfo () {
+        System.out.println("\nAll Vehicles In Storage\n____________________\n");
+        int count = 1;
+        for ( Vehicle vehicle : allVehicles ) {
+
+            int maxPass = vehicle.getMaxPassengers();
+            int currentlyHolding = vehicle.countOccupants();
+            String vehicleType = vehicle.getClass().getSimpleName();
+
+            String display =
+                    MessageFormat.format("{0}) Name: {1}, Type: {2}, Maximum Passengers: {3}, Currently Holding: {4}",
+                        count, vehicle.name, vehicleType, maxPass, currentlyHolding );
+
+            System.out.println(display);
+
+            count++;
+        }
+        System.out.println("____________________\n");
+    }
+
+    public void showAllOccupantInfo () {
+
+        System.out.println("\nAll Vehicle Operators And Passengers\n____________________\n");
+        int count = 1;
+        for ( Occupant occupant : allOccupants ) {
+
+            String vehicleType = occupant.occupying.getClass().getSimpleName();
+            String vehicleName = occupant.occupying.name;
+            String role = occupant.getClass().getSimpleName();
+            String display =
+                    MessageFormat.format("{0}) Name: {1}, In Vehicle Type: {2}, Vehicle Name: {3}, Role: {4}",
+                            count, occupant.name, vehicleType, vehicleName, role );
+
+            System.out.println(display);
+
+            count++;
+        }
+        System.out.println("____________________\n");
+
+    }
+
 }
