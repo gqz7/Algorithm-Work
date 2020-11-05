7759f27722d3e8b56dcf6cbd70d3965f75bbd68a
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-05 16:16:39 -0500

    working with a new Generic class exampling how to add more than one generic param

---
+++ Java/AdvJavaConcepts/Generics/KeyValuePair.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class KeyValuePair<K, V> {
+
+    private K key;
+    private V value;
+
+    public KeyValuePair( K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+}
