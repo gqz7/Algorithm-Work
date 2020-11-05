a73c279bd42f4855b84442c512f277e8a6343e2f
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-05 17:21:32 -0500

    show an example of what a foreach loop is actually doing

---
+++ Java/AdvJavaConcepts/Collections/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var myStringList = new GenericList<String>(3);
+
+        myStringList.add("hello");
+        myStringList.add("world");
+
+        Iterator<String> iterator = myStringList.iterator();
+
+        //what a for each loop is actually doing
+        while (iterator.hasNext()) {
+            String current = iterator.next();
+            System.out.print(current+" ");
+        }
+        System.out.println();
+        //this code is often replaced by a foreach loop that the compiler will end up replacing with the code above
+        for ( String word : myStringList) {
+            System.out.print( word +" ");
+        }
+
+    }
+}
