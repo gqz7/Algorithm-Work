e4782e5bacdd51b19fce1a5c75060e0b2ba0551d
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-05 22:18:28 -0500

    update investment accounts and create sub-class of investment. a cd account

--- Java/OOP-Basics/Bank/Account/InvestmentAccount.java
+++ Java/OOP-Basics/Bank/Account/InvestmentAccount.java
@@ -2,16 +2,30 @@ package Account;
 
 public class InvestmentAccount extends BankAccount {
 
-    private int period = 3; //represent the time of investment in months
-    private int interest = 5; //how much you gain from your investment every period
+    private final static int investmentWithdrawFee = 1000;
 
-    public InvestmentAccount( String owner ) {
+    private int period = 3; //represent the time of investment in months
+    private int interest; //how much you gain from your investment every period
+
+    public InvestmentAccount( String owner, int interest ) {
         super (owner, "Investing");
+        this.interest = interest;
+
+    }
+
+    public InvestmentAccount( String owner, int interest, String type ) {
+        super (owner, type);
+        this.interest = interest;
 
     }
 
     public void applyInvestment() {
         balance += (int) ((balance * interest) / 100);
+    }
+
+    @Override
+    public void withdraw (int amount ){
+        super.withdraw( amount + investmentWithdrawFee);
     }
 
 }
