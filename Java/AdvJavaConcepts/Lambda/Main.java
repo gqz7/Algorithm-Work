b0f5de17173a0d6a94695295a13c5687b821d572
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-07 13:31:36 -0500

    run all examples from main method

---
+++ Java/AdvJavaConcepts/Lambda/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Lambdas_Examples.show();
+        Consumer_Interface_Examples.show();
+        Supplier_Interface_Examples.show();
+        Function_Interface_Examples.show();
+        Predicate_Interface_Examples.show();
+        Specialized_Operator_Interface_Example.show();
+    }
+}
