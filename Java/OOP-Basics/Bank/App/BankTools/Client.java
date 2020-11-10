2572073fa5248d93c9578ebdd57845f3d22bf18f
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-09 23:37:50 -0500

    create class for clients so store common account info

---
+++ Java/OOP-Basics/Bank/App/BankTools/Client.java
@@ -0,0 +1,15 @@
+package BankTools;
+
+import Account.Account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+
+    private final List<Account> accounts;
+
+    public Client () {
+        accounts = new ArrayList<>();
+    }
+}
