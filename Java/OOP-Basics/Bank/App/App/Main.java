a460376faeb4681c1e2e23d1adab76fe50cba9b4
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-09 23:33:57 -0500

    create package for Application basics

---
+++ Java/OOP-Basics/Bank/App/App/Main.java
@@ -0,0 +1,45 @@
+package App;
+
+import Account.CDInvestment;
+import Account.SavingsAccount;
+import BankTools.DebitCard;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //TESTING UNIQUE ACCOUNT NUMBER GENERATION
+//        for (int i = 0; i < 10; i++) {
+//            String fName = Integer.toString(i);
+//            String lName = Integer.toString(i+1);
+//            var myAccount = new BankAccount(fName, lName);
+//        }
+//
+//        BankAccount.showAllNumbers();
+//            var myAccount = new BankAccount("Test first", "Test last");
+//            myAccount.getAccountBalance();
+
+//        var mySavings = new SavingsAccount("bill", 3);
+//        mySavings.deposit(100_000_000);
+//        for (int i = 0; i < 7; i++) {
+//            mySavings.withdraw(115);
+//            mySavings.getAccountBalance();
+//            System.out.println(mySavings.getBalance());
+//        }
+//        var myInvestment = new InvestmentAccount("Gavin", 10);
+//        myInvestment.deposit(10000);
+//        myInvestment.applyInvestment();
+//        System.out.println(myInvestment);
+
+        var myCD = new CDInvestment("Tim", 12);
+        myCD.deposit(10000);
+        myCD.applyInvestment();
+//        System.out.println(myCD);
+        var mySavings = new SavingsAccount("Tim", 10);
+
+        DebitCard myDebit = new DebitCard("2345");
+        myDebit.addAtmAccounts(myCD);
+        myDebit.addAtmAccounts(mySavings);
+
+        myDebit.displayAccounts();
+    }
+}
