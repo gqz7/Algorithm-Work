3d9fb3701f3fe40f0fe908d60ff5a5074c6f699c
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-10 19:06:05 -0500

    introduce a CLI script for taking in user input from the command-line, comment out old test in Main method

--- Java/OOP-Basics/Bank/App/Main.java
+++ Java/OOP-Basics/Bank/App/Main.java
@@ -1,8 +1,8 @@
 package App;
 
 import Account.CDInvestment;
-import Account.InvestmentAccount;
 import Account.SavingsAccount;
+import BankTools.DebitCard;
 
 public class Main {
 
@@ -25,15 +25,24 @@ public class Main {
 //            mySavings.getAccountBalance();
 //            System.out.println(mySavings.getBalance());
 //        }
+
+
+
 //        var myInvestment = new InvestmentAccount("Gavin", 10);
 //        myInvestment.deposit(10000);
 //        myInvestment.applyInvestment();
 //        System.out.println(myInvestment);
 
-        var myCD = new CDInvestment("Tim", 12);
-        myCD.deposit(10000);
-        myCD.applyInvestment();
-        System.out.println(myCD);
-
+//        var myCD = new CDInvestment("Tim", 12);
+//        myCD.deposit(10000);
+//        myCD.applyInvestment();
+////        System.out.println(myCD);
+//        var mySavings = new SavingsAccount("Tim", 10);
+//
+//        DebitCard myDebit = new DebitCard("2345");
+//        myDebit.addAtmAccounts(myCD);
+//        myDebit.addAtmAccounts(mySavings);
+//
+//        myDebit.displayAccounts();
     }
 }
