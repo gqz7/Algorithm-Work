fc94d43147ea404b90e76098fedb41197a46f180
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-10 19:05:21 -0500

    create a card abstract class for the client to have a list of Cards, debit card is the only child class right now

---
+++ Java/OOP-Basics/Bank/BankTools/Card.java
@@ -0,0 +1,4 @@
+package BankTools;
+
+public abstract class Card {
+}
--- Java/OOP-Basics/Bank/BankTools/DebitCard.java
+++ Java/OOP-Basics/Bank/BankTools/DebitCard.java
@@ -1,23 +1,26 @@
 package BankTools;
 
+import Account.Account;
 import Account.CheckingAccount;
 import Account.SavingsAccount;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class DebitCard {
+public class DebitCard extends Card {
     
     private final String pin;
+    private final Client holder;
     private CheckingAccount primary;
     private String securityCode;
     private String cardNum;
     private Calendar expData;
-    private List< CheckingAccount > checkingAccounts;
-    private List<SavingsAccount> savingsAccounts;
+    private List<Account> atmAccounts = new ArrayList<>();
 
-    public DebitCard( String pin ) {
+    public DebitCard( String pin, Client holder ) {
         this.pin = pin;
+        this.holder = holder;
     }
 
     public boolean charge( int amt, String pin) {
@@ -27,4 +30,15 @@ public class DebitCard {
         }
         return false;
     }
+
+    public void addAtmAccounts(Account account ) {
+        atmAccounts.add( account );
+    }
+
+    public void displayAccounts() {
+        for (var account: atmAccounts) {
+            System.out.println(account);
+        }
+    }
+
 }
