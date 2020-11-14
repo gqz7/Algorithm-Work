cc50c004108edafbfc684ec32dbf60f0165a9e08
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-14 15:53:42 -0500

    make player names unique in a game, storing the names in an array list

--- Java/GameProjects/Yahtzee/game_src/gamelogic/YahtzeeGame.java
+++ Java/GameProjects/Yahtzee/game_src/gamelogic/YahtzeeGame.java
@@ -2,6 +2,9 @@ package com.yahtzee.game_src.gamelogic;
 
 import com.yahtzee.game_src.players.YahtzeePlayer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class YahtzeeGame {
 
     private YahtzeePlayer[] players;
@@ -12,7 +15,7 @@ public class YahtzeeGame {
 
     public YahtzeeGame ( ) {
 
-        printWelcomMsg();
+        printWelcomeMsg();
 
         initializeGame();
         
@@ -30,7 +33,7 @@ public class YahtzeeGame {
         YahtzeeScoring.endGameReport(players);
 
         //ask players if they would like to play again, if they do the program will re-initialize the game
-        boolean restartGame = YahtzeeCLI.isPlayingAgain();
+        boolean restartGame = YahtzeeCLI.playingAgain();
 
         if (restartGame) initializeGame();
         //if the user gets here the program will end
@@ -43,25 +46,37 @@ public class YahtzeeGame {
 
         numberOfPlayers = gameData[0];
         numberOfTurns = gameData[1];
-        players = new YahtzeePlayer[numberOfPlayers];
-
-        //setup the players array and allow the players to enter their name
-        for (int i = 0; i < numberOfPlayers; i++) {
-            System.out.println("\nPlayer #" + (i+1) + " enter your name...");
-            String playersName = YahtzeeCLI.getPlayerName();
-            players[i] = new YahtzeePlayer(playersName);
-        }
+        initializePlayers();
 
         playGame();
 
     }
 
+    private void initializePlayers() {
+        players = new YahtzeePlayer[numberOfPlayers];
 
-    private void printWelcomMsg() {
+        List<String> playerNames = new ArrayList<>();
+        //setup the players array and allow the players to enter their name
+        for (int i = 0; i < numberOfPlayers; i++) {
+            String playerName = "";
+            while (true) {
+                System.out.println("\nPlayer #" + (i+1) + " enter your name...");
+                playerName = YahtzeeCLI.getPlayerName();
+                if (playerNames.contains(playerName))
+                    System.out.println("\nSomeone already entered '" + playerName + "', try something else.");
+                else {
+                    playerNames.add(playerName);
+                    break;
+                }
+            }
+            players[i] = new YahtzeePlayer(playerName);
+        }
+    }
+
+    private void printWelcomeMsg() {
 
         System.out.println("\n\n----------------------------------\n|\t\t\tYAHTZEE\t\t\t\t |\n|________________________________|\n");
         System.out.println("Welcome to this Yahtzee Java simulation\n\n");
-
     }
 
 }
