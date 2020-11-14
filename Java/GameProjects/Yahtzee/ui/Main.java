eef32bc752b4281ce448c60280d7e82cf84d62cf
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-14 02:16:18 -0500

    create a UI package that is does not involve any Yahtzee code except creating one instance of YahtzeeGame to start up the game

---
+++ Java/GameProjects/Yahtzee/ui/Main.java
@@ -0,0 +1,10 @@
+package com.yahtzee.ui;
+
+import com.yahtzee.game_src.gamelogic.YahtzeeGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new YahtzeeGame();
+    }
+}
