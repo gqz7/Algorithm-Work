c4fd4983792b3ac0015aa4e2b27e2ac1c4e5fe75
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-13 23:59:39 -0500

    update Die

--- Java/GameProjects/Yahtzee/Die.java
+++ Java/GameProjects/Yahtzee/Die.java
@@ -1,7 +1,25 @@
-package com.starfield;
+package com.yahtzee;
 
 public class Die {
     public int currentValue;
     public int faces;
+
+    public Die (int faces) {
+
+        setFaces(faces);
+        currentValue = 1;
+    }
+
+    public void roll() {
+        currentValue = (int) (faces * Math.random());
+    }
+
+    private void setFaces (int numberOfFaces ) throws IllegalArgumentException {
+        if (numberOfFaces > 0 && numberOfFaces <= 100) {
+            faces = numberOfFaces;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
