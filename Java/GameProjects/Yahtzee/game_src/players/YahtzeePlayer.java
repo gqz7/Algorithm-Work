78d9bfc96992be888d9853c5d2522e11655c5c4b
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-16 07:40:05 -0500

    flesh of the takeTurn method to allow players to end their turn before all rolls have finished, account for the possiblity of no scoring options being available, handled all edge cases I could think of

--- Java/GameProjects/Yahtzee/game_src/players/YahtzeePlayer.java
+++ Java/GameProjects/Yahtzee/game_src/players/YahtzeePlayer.java
@@ -8,9 +8,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class YahtzeePlayer {
-
-    private static final int numberOfDicePerPlayer = 5;
-    private static final int numberOfFacesPerDie = 6;
+    private static final int MAX_ROLLS = 3;
+    private static final int NUMBER_OF_DICE_PER_PLAYER = 5;
+    private static final int NUMBER_OF_FACES_PER_DIE = 6;
 
     private final DiceRoller hand;
     private final ScoreCard card;
@@ -19,16 +19,16 @@ public class YahtzeePlayer {
     public YahtzeePlayer (String name) {
         this.name = name;
         card = new ScoreCard();
-        hand = new DiceRoller(numberOfDicePerPlayer, numberOfFacesPerDie);
+        hand = new DiceRoller(NUMBER_OF_DICE_PER_PLAYER, NUMBER_OF_FACES_PER_DIE);
     }
 
     public void takeTurn() {
-        System.out.println("\n______________________\nYour turn " + name + "!\n");
+        System.out.println("\n-----------------------------\n\tYour turn " + name + "!\n_____________________________\n");
 
         //store which rolls the play wants to keep and which to re-roll;
         List<Integer> keepRolling = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4));
 
-        for (int rolls = 1; rolls <= 3; rolls++) {
+        for (int rolls = 1; rolls <= MAX_ROLLS; rolls++) {
             System.out.println("\n\nRoll #" + rolls + "\nRolling...\n");
 
             //rolling dice, because they are might not all be rolled,
@@ -36,11 +36,34 @@ public class YahtzeePlayer {
             for ( int index : keepRolling ) { hand.rollDie(index); }
 
             int[] values = hand.getValues();
-            System.out.println(name + ", you rolled: " + Arrays.toString(values));
+            System.out.println(name + ", you rolled: \n" + Arrays.toString(values) + "\n");
 
-            if (rolls == 3)
-                pickTurnScore();
-            else {
+            //allow the user to end their turn prematurely
+            boolean endTurn = false;
+            //this check that it makes sense to ask them if they want to end their turn,
+            // if they're on their last roll it makes no sense to ask this question
+            if (rolls != MAX_ROLLS) {
+                endTurn = YahtzeeCLI.endTurn();
+            }
+
+
+            if (rolls == MAX_ROLLS || endTurn) { //end the turn, either by choice or because rolls are done
+
+                //i updated this method to return a boolean because if a user has more rolls
+                // but they decide to end their turn early and they have no option it would be nice to be allowed to roll again
+                // rather than be given a zero because you thought you would have options when you didn't
+                //if this boolean is false that means they were not able to make a selection because there were no options
+                boolean hadOptions = pickTurnScore();
+
+                if (!hadOptions && endTurn ) { //only if they have more rolls left will they see this
+                    System.out.println("\nYou are ouf of options right now, but you can roll again");
+                } else if ( !hadOptions ) {
+                    System.out.println("\nSorry your all out of options this round, better luck next time!");
+                } else {
+                    rolls = MAX_ROLLS; //this will get triggered if one ends their turn early and they made a selection
+                }
+
+            } else {
                 keepRolling.clear();
                 for ( int i = 0; i < values.length; i++ )  {
                     int rolledNumber = values[i];
@@ -52,23 +75,26 @@ public class YahtzeePlayer {
 
         }
 
-        System.out.println("Your turn is over " + name);
+        System.out.println("Great job " + name + "! Your turn is now over.");
 
     }
 
-    private void pickTurnScore() {
+    private boolean pickTurnScore() {
         //use the current state of hand to determine this turns score and update the scorecard accordingly
         String[] comboChoices = card.getPossibleCombosChoices( hand.getValues() );
+
+        //if a player doesn't have any possible choices they get 0 points, ie the 'makeChoice' method doesn't need to run
+        if (comboChoices.length == 0) {
+            return false;
+        }
 
         //this needs a minus one because the index will be minus one from their selection
         int comboIndex = YahtzeeCLI.chooseCombo(comboChoices) - 1;
 
         card.makeChoice( comboChoices[comboIndex] );
 
-    }
+        return true;
 
-    public String getName() {
-        return name;
     }
 
     public ScoreCard getCard() {
