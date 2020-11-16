92525a7310aeaf047e17d15c8b762848117d11a3
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-16 07:41:37 -0500

    finalize the end of game console logs, fix bug that would always declare the last player the winner (added line 24), add in some comments throughout

--- Java/GameProjects/Yahtzee/game_src/players/YahtzeeScoring.java
+++ Java/GameProjects/Yahtzee/game_src/players/YahtzeeScoring.java
@@ -1,33 +1,42 @@
 package com.yahtzee.game_src.players;
 
-import com.yahtzee.game_src.players.ScoreCard;
-import com.yahtzee.game_src.players.YahtzeePlayer;
-
 import java.util.*;
 
 public class YahtzeeScoring {
 
     public static void endGameReport(YahtzeePlayer[] allPlayers) {
 
+        System.out.println("\n______________________________________\n");
+        System.out.println("The Game Is Over! Let's See Who Won\n\n\tSCORES:\n");
+
         int highestScore = 0;
 
         List<YahtzeePlayer> winners = new ArrayList<>();
 
-        for ( var player: allPlayers ) {
+        //iterate through all players in the game
+        for ( YahtzeePlayer player: allPlayers ) {
 
+            //get the players total score
             int finalScore = tallyScore(player.getCard());
 
+            //check if the player got the highest score or tied
             if (finalScore > highestScore) {
+                highestScore = finalScore;
                 winners.clear();
                 winners.add(player);
 
             } else if (finalScore == highestScore) winners.add(player);
 
-            System.out.println(player + ": " + finalScore);
+            //print out each player and their score (in the order the players were entered)
+            System.out.println("\t" + player + ": " + finalScore);
 
         }
 
-        System.out.println("The Winners:\n\n" + Arrays.toString(winners.toArray()));
+        //PRINT OUT THE WINNERS
+        System.out.println("\n______________________________________\n");
+        System.out.println( "\nThe " + ( winners.size() > 1 ? "Winners Are" : "Winner Is" ) + "...\n");
+        for ( YahtzeePlayer name : winners) System.out.println("\t" + name);
+        System.out.println("\n\n______________________________________\n");
 
     }
 
@@ -36,11 +45,15 @@ public class YahtzeeScoring {
         int upScore = 0;
         int lwScore = 0;
 
+        //convert the Combo HashMap into something iterable
         for ( Map.Entry comboEntry : scoreCard.getCombinations().entrySet() ) {
 
+            //get the value of the HM, the Combo object
             ScoreCard.Combo combo = (ScoreCard.Combo) comboEntry.getValue();
 
+            //if the combo has been used it was at once point a selection by that player in the game
             if (combo.getHasBeenUsed()) {
+                //get the points and add to the right section
                 int comboScore = combo.getPointsValue();
                 if (combo.getIsUpper())
                     upScore += comboScore;
@@ -50,10 +63,10 @@ public class YahtzeeScoring {
             }
 
         }
-
+        //add up the Yahtzee bonuses
         lwScore += scoreCard.getYahtzeeBonuses() * 100;
-
-        if ( upScore > 63 ) upScore += 35;
+        //add the upper section bonus if they got it
+        if ( upScore >= 63 ) upScore += 35;
 
         return lwScore + upScore;
     }
