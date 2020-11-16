946e80f896c5c74cbacc78dbc64224b53c76dbfa
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-16 07:38:22 -0500

    implement all the scoring combinations into the getPossibleCombosChoices method, update keys to use the enums instead of string literals, also add a lot of comments and remove unneeded methods from both ScoreCard and Combo

--- Java/GameProjects/Yahtzee/game_src/players/ScoreCard.java
+++ Java/GameProjects/Yahtzee/game_src/players/ScoreCard.java
@@ -1,16 +1,38 @@
 package com.yahtzee.game_src.players;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.yahtzee.game_src.ComboKeys;
+
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class ScoreCard {
 
+    /*
+        The ScoreCard is what keeps a record of the players game, the main piece of data here is the HashMap that scores Combos
+        each Combo represents the 13 possible score combinations you can get in Yahtzee
+        each as a key attached to them in the HashMap. At first I just entered the string literals,
+        then I tried enums but didn't like how mess the code got.
+        finally I settled with static constants which is much safer than string literals all over.
+        my only problem with this approach is that it makes the top of this class look a bit messy
+    */
+
+    private static final String ACES = ComboKeys.ACES.toString();
+    private static final String TWOS = ComboKeys.TWOS.toString();
+    private static final String THREES = ComboKeys.THREES.toString();
+    private static final String FOURS = ComboKeys.FOURS.toString();
+    private static final String FIVES = ComboKeys.FIVES.toString();
+    private static final String SIXES = ComboKeys.SIXES.toString();
+    private static final String TOAK = ComboKeys.TOAK.toString();
+    private static final String FOAK = ComboKeys.FOAK.toString();
+    private static final String FLHO = ComboKeys.FLHO.toString();
+    private static final String CHNC = ComboKeys.CHNC.toString();
+    private static final String SMST = ComboKeys.SMST.toString();
+    private static final String LGST = ComboKeys.LGST.toString();
+    private static final String YATZ = ComboKeys.YATZ.toString();
+
     //used in possibleCombos method to associate upper-section combos with an int/index
-    private static final String[] upperSectionKeys = {"aces", "twos", "threes", "fours", "fives", "sixes"};
+    private static final String[] upperSectionKeys = {ACES, TWOS, THREES, FOURS, FIVES, SIXES};
+
     //this class is centered around this hash-map that is a record keeper for all the players turns and the choices they've made
     private final Map<String, Combo> combinations = new HashMap<>();
 
@@ -27,10 +49,18 @@ public class ScoreCard {
     public String[] getPossibleCombosChoices(int[] numbers ) {
 
         //it can't be known from the start of this method how many combos will be possible so its best to use a List
-        List<String> combos = new ArrayList<>();
+        Set<String> combos = new HashSet<>();
 
-        //CHECK POSSIBLE UPPER-SECTION COMBOS
+        //this boolean get used to check for a Full-house (comments below explain further)
+        boolean fourOfAKindPresent = false;
+
+        //total sum of number gets used by Chance, 3-of-a-Kind, and 4-of-a-Kind.
+        //that's why it makes sense to declare it here and not run the same code potentially 3 times
+        int totalSum = IntStream.of(numbers).sum();
+
+        //CHECK POSSIBLE UPPER-SECTION COMBOS (and any that revolve around repeat numbers)
         for (int num = 0; num < 6; num++) {
+
             int lookingFor = num+1;
             //create an array of just a single int (1-6) in the given hand
             int[] foundNumbers = IntStream.of(numbers).filter(x -> x == lookingFor).toArray();
@@ -39,56 +69,104 @@ public class ScoreCard {
             String comboKey = upperSectionKeys[num];
 
             //grab the combo associated to the number
-            Combo possibleCombo = combinations.get(comboKey);
+            Combo upperSectionCombo = combinations.get(comboKey);
 
             //if there are any matches of a number 1-6 then allow the counting of that number as a score.
-            if ( foundNumbers.length > 0 && !possibleCombo.getHasBeenUsed()) {
+            if ( foundNumbers.length > 0 && !upperSectionCombo.getHasBeenUsed()) {
                 //this value can be overwritten in the next turn if not selected
-                possibleCombo.setPointsValue(
-                    //this is a simpler version of 'IntStream' line below because all the numbers in the array should be the same because of th .filter
-                    foundNumbers.length * lookingFor
-                    //IntStream.of(foundNumbers).sum() //adds all numbers together found in the DiceRoller after the turn
+                upperSectionCombo.setPointsValue(
+                        //this is a simpler version of 'IntStream' line below because all the numbers in the array should be the same because of th .filter
+                        foundNumbers.length * lookingFor
+                        //IntStream.of(foundNumbers).sum() //adds all unique numbers together
                 );
                 //add the possible selection to the list of choices the player can then choose from after this method runs
                 combos.add( comboKey );
             }
 
-            if ( foundNumbers.length == 5 ) {
-                //yahtzee!
+            //Check 3 of a Kind
+            Combo threeOfAKindCombo = combinations.get(TOAK);
+            if ( foundNumbers.length >= 3 && !threeOfAKindCombo.getHasBeenUsed()) {
+                threeOfAKindCombo.setPointsValue(totalSum);
+                combos.add(TOAK);
+            }
+
+            //Check 4 of a Kind
+            Combo fourOfAKindCombo = combinations.get(FOAK);
+            if ( foundNumbers.length >= 4) {
+                fourOfAKindPresent = true; //set this boolean to true so the Full-House check can utilize it
+                if (!fourOfAKindCombo.getHasBeenUsed()) {
+                    fourOfAKindCombo.setPointsValue(totalSum);
+                    combos.add(FOAK);
+                }
+            }
+
+            //Check YAHTZEE
+            //the length can only be 5 if all the numbers are the same, hence YAHTZEE
+            //no need to check if this combo has been used because Yahtzee can be used more than once
+            //the logic for handling more than one Yahtzee occurrence is in the 'makeChoice' method
+            if ( foundNumbers.length == 5 ) combos.add(YATZ); //add the choice to choose Yahtzee
+
+        }
+
+        //CHECK REMAINING POSSIBLE LOWER SECTION COMBOS
+
+        //Calculate the max amount of consecutive numbers in the hand, this will be used for the 'straights'
+        int[] sortedNumbers = Arrays.stream(numbers).sorted().toArray();
+        int consecutiveNumbers = 1;
+        int maxInARow = 1;
+        for (int i = 1; i < sortedNumbers.length; i++) {
+            if (sortedNumbers[i] -1 == sortedNumbers[i-1]) {
+                consecutiveNumbers++;
+                if (consecutiveNumbers > maxInARow ) maxInARow = consecutiveNumbers;
+            } else {
+                consecutiveNumbers = 1;
             }
         }
 
-        //CHECK POSSIBLE LOWER SECTION COMBOS
+        //Check Small Straight
+        //no points need to be set since 'straights' give a static number of points
+        if (maxInARow >= 4) combos.add(SMST);
 
-        //check small straight
-
-
-        //check large straight
-
-
-        //check 3 of a kind
+        //Check Large Straight
+        if (maxInARow == 5) combos.add(LGST);
 
 
-        //check 4 of a kind
+        /* Check Full House
+            this one may be possible to check Full-House in the 'num' for-loop but I thought this way was clean and pretty clever
+            essentially this uses a stream to reduces the numbers array to it unique values and count how many are present
+            for a full-house there should only be two unique values. there should also not be a 4 of a Kind present,
+            so the program just simply checks that 4 of a kind is not present
+            finally this can only be used once so that also needs to be checked
+        */
+        if (
+                !fourOfAKindPresent
+                        && Arrays.stream(numbers).distinct().count() == 2
+                        && !( combinations.get(FLHO).getHasBeenUsed() )
+        ) combos.add(FLHO); //full house has a static pointsValues so it just needs to be added to the HashSet
 
-        //check full house
 
         //change can always be used as long as it hasn't been used already
-        Combo chanceCombo = combinations.get("cnc");
-        if ( !chanceCombo.hasBeenUsed ) {
+        Combo chanceCombo = combinations.get(CHNC);
+        if ( !chanceCombo.getHasBeenUsed() ) { //if the combo has NOT been used
             //calculate the sum of all numbers in roll
-            int chanceTotal = IntStream.of(numbers).sum();
             //set the points value
-            chanceCombo.setPointsValue(chanceTotal);
+            chanceCombo.setPointsValue(totalSum);
             //add to possible selections
-            combos.add("cnc");
+            combos.add(CHNC);
         }
 
-        return combos.toArray(String[]::new);
+        return combos.toArray(String[]::new); //convert the combos from a HashSet to an Array
     }
 
     //make protected after testing
     public void makeChoice (String key) {
+        //first checks if the choice was a YAHTZEE,
+        // if they have already gotten a YAHTZEE the hash map should not be altered,
+        // the yahtzeeBonus int should be incremented instead
+        if (key.equals(YATZ) && combinations.get(YATZ).getHasBeenUsed()) {
+            yahtzeeBonuses++;
+            return;
+        }
         //by running this method the current pointsValue will be locked in for this Combo and cant be changed
         combinations.get(key).useCombo();
     }
@@ -107,28 +185,20 @@ public class ScoreCard {
         private final boolean isUpper;
         //in Yahtzee a combo can only be used once per game, hence the boolean
         private boolean hasBeenUsed;
-        //this many be redundant
-        private final String key;
         //each combo has different points, some are determined by the roll, some are static
         private int pointsValue;
 
-        protected Combo( boolean isUpper, String key) {
+        protected Combo( boolean isUpper) {
             this.isUpper = isUpper;
             this.pointsValue = 0;
-            this.key = key;
             hasBeenUsed = false;
         }
 
         //OVERLOAD - this is for the combos that will have static pointValue
-        protected Combo( boolean isUpper, String key, int pointsValue) {
+        protected Combo( boolean isUpper, int pointsValue) {
             this.pointsValue = pointsValue;
             this.isUpper = isUpper;
-            this.key = key;
             hasBeenUsed = false;
-        }
-
-        protected String getKey() {
-            return key;
         }
 
         protected boolean getIsUpper() {
@@ -151,19 +221,19 @@ public class ScoreCard {
     // there isn't any fancy algorithmic way of creating this as far as I know. So I've hard coded them here
     private void initializeComboMap() {
         //UPPER
-        combinations.put("aces", new Combo(true, "aces"));
-        combinations.put("twos", new Combo(true, "twos"));
-        combinations.put("threes", new Combo(true, "threes"));
-        combinations.put("fours", new Combo(true, "fours"));
-        combinations.put("fives", new Combo(true, "fives"));
-        combinations.put("sixes", new Combo(true, "sixes"));
+        combinations.put(ACES, new Combo(true));
+        combinations.put(TWOS, new Combo(true));
+        combinations.put(THREES, new Combo(true));
+        combinations.put(FOURS, new Combo(true));
+        combinations.put(FIVES, new Combo(true));
+        combinations.put(SIXES, new Combo(true));
         //LOWER (some combos in the lower section have static point values to those are set by the constructor)
-        combinations.put("smS", new Combo(false, "smS", 30));
-        combinations.put("lgS", new Combo(false, "lgS", 40));
-        combinations.put("fh",  new Combo(false, "fh", 25));
-        combinations.put("yah", new Combo(false, "yah", 50));
-        combinations.put("cnc", new Combo(false, "cnc"));
-        combinations.put("3ok", new Combo(false, "3ok"));
-        combinations.put("4ok", new Combo(false, "4ok"));
+        combinations.put(CHNC, new Combo(false));
+        combinations.put(TOAK, new Combo(false));
+        combinations.put(FOAK, new Combo(false));
+        combinations.put(SMST, new Combo(false, 30));
+        combinations.put(LGST, new Combo(false, 40));
+        combinations.put(FLHO,  new Combo(false, 25));
+        combinations.put(YATZ, new Combo(false, 50));
     }
 }
