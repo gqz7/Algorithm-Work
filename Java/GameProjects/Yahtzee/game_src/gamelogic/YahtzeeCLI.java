2fded513666beb54938d206705306e954176c35f
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-16 07:33:17 -0500

    add in a question about ending your turn, use best naming practices for constant values

--- Java/GameProjects/Yahtzee/game_src/gamelogic/YahtzeeCLI.java
+++ Java/GameProjects/Yahtzee/game_src/gamelogic/YahtzeeCLI.java
@@ -1,5 +1,6 @@
 package com.yahtzee.game_src.gamelogic;
 
+import com.yahtzee.game_src.ComboKeys;
 import com.yahtzee.ui.CLI;
 
 import java.util.HashMap;
@@ -7,17 +8,18 @@ import java.util.Map;
 
 public class YahtzeeCLI {
 
-    private final static String numOfTurnsQuestion = "\nHow many turns would you like this game of Yahtzee to be? (13 is recommended)";
-    private final static String numOfPlayersQuestion = "\nHow many players would you like this game of Yahtzee to have?";
-    private final static String restartQuestion = "\nThat was fun! Would you like to play another game?";
-    private final static String keepRollingDieQuestion = "Would you like to keep rolling ";
-    private final static String comboQuestion = "Choose one of the scoring combinations above";
-    private final static String nameQuestion = "What what is your name?";
+    private final static String NUM_OF_TURNS_QUESTION = "\nHow many turns would you like this game of Yahtzee to be? (13 is recommended)";
+    private final static String NUM_OF_PLAYERS_QUESTION = "\nHow many players would you like this game of Yahtzee to have?";
+    private final static String RESTART_QUESTION = "\nThat was fun! Would you like to play another game?";
+    private final static String KEEP_ROLLING_DIE_QUESTION = "Would you like to keep rolling ";
+    private final static String COMBO_QUESTION = "Choose one of the scoring combinations above";
+    private final static String NAME_QUESTION = "What what is your name?";
+    private final static String END_TURN_QUESTION = "Would you like to end your turn now?";
 
-    private final static int maxTurns = 30;
-    private final static int minTurns = 1;
-    private final static int maxPlayers = 10;
-    private final static int minPlayers = 2;
+    private final static int MAX_TURNS = 13;
+    private final static int MIN_TURNS = 1;
+    private final static int MAX_PLAYERS = 10;
+    private final static int MIN_PLAYERS = 2;
 
     public static int[] getGameData() {
         return new int[] {
@@ -27,50 +29,77 @@ public class YahtzeeCLI {
     }
 
     private static int getNumberOfTurns() {
-        return CLI.numberIntQuestion( numOfTurnsQuestion, minTurns, maxTurns );
+        return CLI.numberIntQuestion(NUM_OF_TURNS_QUESTION, MIN_TURNS, MAX_TURNS);
     }
 
     private static int getNumberOfPlayers() {
-        return CLI.numberIntQuestion( numOfPlayersQuestion, minPlayers, maxPlayers );
+        return CLI.numberIntQuestion(NUM_OF_PLAYERS_QUESTION, MIN_PLAYERS, MAX_PLAYERS);
     }
 
     public static String getPlayerName() {
-        return CLI.stringQuestion(nameQuestion);
+        return CLI.stringQuestion(NAME_QUESTION);
     }
 
     public static boolean playingAgain() {
-        return CLI.yesOrNo(restartQuestion);
+        return CLI.yesOrNo(RESTART_QUESTION);
     }
 
-    public static boolean keepRollingNumber(int num ) { return CLI.yesOrNo(keepRollingDieQuestion + num + "?"); }
+    public static boolean keepRollingNumber(int num ) { return CLI.yesOrNo(KEEP_ROLLING_DIE_QUESTION + num + "?"); }
 
     public static int chooseCombo (String[] choices) {
-        System.out.println("\n\t- Select A Scoring Combination -");
+        System.out.println("\n- Select A Scoring Combination -\n");
         for (int i = 0; i < choices.length; i++) {
             String displayText = COMBO_MAP.get( choices[i] );
             System.out.println( (i+1) + ") " + displayText);
         }
 
-        return CLI.numberIntQuestion(comboQuestion, 1, choices.length);
+        return CLI.numberIntQuestion(COMBO_QUESTION, 1, choices.length);
     }
+
+    private static final String ACES = ComboKeys.ACES.toString();
+    private static final String TWOS = ComboKeys.TWOS.toString();
+    private static final String THREES = ComboKeys.THREES.toString();
+    private static final String FOURS = ComboKeys.FOURS.toString();
+    private static final String FIVES = ComboKeys.FIVES.toString();
+    private static final String SIXES = ComboKeys.SIXES.toString();
+    private static final String TOAK = ComboKeys.TOAK.toString();
+    private static final String FOAK = ComboKeys.FOAK.toString();
+    private static final String FLHO = ComboKeys.FLHO.toString();
+    private static final String CHNC = ComboKeys.CHNC.toString();
+    private static final String SMST = ComboKeys.SMST.toString();
+    private static final String LGST = ComboKeys.LGST.toString();
+    private static final String YATZ = ComboKeys.YATZ.toString();
 
     //this hash map get used when a player is selecting a choice.
     // the method will be given keys from ScoreCard and passed to the player, then to this CLI
     private static final Map<String, String> COMBO_MAP = new HashMap<>() {{
         //upper
-        put("aces", "Total of Aces");
-        put("twos", "Total of Twos");
-        put("threes", "Total of Threes");
-        put("fours", "Total of Fours");
-        put("fives", "Total of Fives");
-        put("sixes", "Total of Sixes");
+        put(ACES, "Total of Aces");
+        put(TWOS, "Total of Twos");
+        put(THREES, "Total of Threes");
+        put(FOURS, "Total of Fours");
+        put(FIVES, "Total of Fives");
+        put(SIXES, "Total of Sixes");
         //lower
-        put("smS", "Small Straight");
-        put("lgS", "Large Straight");
-        put("fh", "Full House");
-        put("yah", "YAHTZEE");
-        put("cnc", "Chance (sum of all dice)");
-        put("3ok", "Three of a Kind");
-        put("4ok", "Four of a Kind");
+        put(SMST, "Small Straight");
+        put(LGST, "Large Straight");
+        put(FLHO, "Full House");
+        put(YATZ, "YAHTZEE");
+        put(CHNC, "Chance (sum of all dice)");
+        put(TOAK, "Three of a Kind");
+        put(FOAK, "Four of a Kind");
     }};
+
+    public static boolean endTurn() {
+        return CLI.yesOrNo(END_TURN_QUESTION);
+    }
+
+    public static void printWelcomeMsg() {
+        System.out.println("\n\n----------------------------------\n|\t\t\tYAHTZEE\t\t\t\t |\n|________________________________|\n");
+        System.out.println("Welcome to this Yahtzee Java simulation\n\n");
+    }
+
+    public static void printExitMsg() {
+        System.out.println("\n\n\nThank you for using this Yahtzee simulation, hope you enjoyed!!\n\nprogram ending...");
+    }
 }
