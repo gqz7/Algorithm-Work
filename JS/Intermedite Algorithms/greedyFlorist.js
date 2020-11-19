fbcac534f3d071edd2e828bc1f4116d349fcd748
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-18 20:52:59 -0500

    greedy florist algorithm, provided by HackerRank

---
+++ JS/Intermedite Algorithms/greedyFlorist.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the getMinimumCost function below.
+function getMinimumCost(k, c) {
+
+
+  console.log(k,c);
+}
+
+function main() {
+    // const text = fs.readFileSync("./florist.txt");
+    // const ws = fs.createWriteStream(text);
+
+    const nk = readLine().split(' ');
+
+    const k = parseInt(nk[1], 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    const minimumCost = getMinimumCost(k, c);
+
+    console.log("The minimum cost is: " + minimumCost);
+}
