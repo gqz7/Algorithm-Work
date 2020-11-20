1e1cb2e23f0ceec29a9537a228c1d8366da0833d
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-19 22:24:40 -0500

    Yahtzee player is now an abstract class, make two methods abstract to be implemented diffrently from AI to human. the current logic in there in now moved to the human player, AI will be built from scratch mostly but still have the same functionality as the human

--- Java/GameProjects/Yahtzee/game_src/players/YahtzeePlayer.java
+++ Java/GameProjects/Yahtzee/game_src/players/YahtzeePlayer.java
@@ -8,16 +8,15 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class YahtzeePlayer {
-    private static final int MAX_ROLLS = 3;
-    private static final int NUMBER_OF_DICE_PER_PLAYER = 5;
-    private static final int NUMBER_OF_FACES_PER_DIE = 6;
-
-    private final boolean isDiceSelectionIndividual; 
-    private final DiceRoller hand;
-    private final ScoreCard card;
-    private final String name;
-    private int score;
+public abstract class YahtzeePlayer {
+    protected static final int MAX_ROLLS = 3;
+    protected static final int NUMBER_OF_DICE_PER_PLAYER = 5;
+    protected static final int NUMBER_OF_FACES_PER_DIE = 6;
+    protected final boolean isDiceSelectionIndividual;
+    protected final DiceRoller hand;
+    protected final ScoreCard card;
+    protected final String name;
+    protected int score;
 
     public YahtzeePlayer (String name, boolean isDiceSelectionIndividual) {
         this.name = name;
@@ -27,93 +26,11 @@ public class YahtzeePlayer {
         hand = new DiceRoller(NUMBER_OF_DICE_PER_PLAYER, NUMBER_OF_FACES_PER_DIE);
     }
 
-    public void takeTurn() {
-        System.out.println("\n-----------------------------\n\tYour turn " + name + "!\n_____________________________\n");
+    public abstract void takeTurn();
 
-        System.out.println("Current Score: " + score);
+    public abstract boolean pickTurnScore();
 
-        //store which rolls the play wants to keep and which to re-roll;
-        List<Integer> keepRolling = new ArrayList<>();
-
-        for (int rolls = 1; rolls <= MAX_ROLLS; rolls++) {
-            System.out.println("\n\nRoll #" + rolls + "\nRolling...\n");
-
-            //roll all dice and pass indices of die/dice that should not be rolled
-            hand.rollAllDice(keepRolling);
-
-            int[] currentRollsValues = hand.getValues();
-
-            YahtzeeCLI.displayCurrentRoll(name, currentRollsValues);
-
-            //this will capture if the players turn is over, if rolls is not already at its limit, it will be set to it
-            if (isTurnOver(rolls)) {
-                rolls = MAX_ROLLS;
-            } else {
-
-                keepRolling.clear();
-
-                if (isDiceSelectionIndividual) {
-                    for (int i = 0; i < currentRollsValues.length; i++ )  {
-                        int rolledNumber = currentRollsValues[i];
-                        boolean holdDieNumber = YahtzeeCLI.keepRollingNumber(rolledNumber);
-                        if (!holdDieNumber)
-                            keepRolling.add(i);
-                    }
-                } else { //in this case the player will select the dice they want to re-roll as one input string
-                    YahtzeeCLI.showDiceSelections(currentRollsValues);
-                    String usersChoices =  YahtzeeCLI.getNumbersToKeepRolling();
-                    List<Integer> keepers = parseMultipleDiceInput(usersChoices);
-                    keepRolling.addAll(keepers);
-
-                }
-            }
-        }
-
-        System.out.println("Great job " + name + "! Your turn is now over.");
-
-    }
-
-    private boolean pickTurnScore() {
-        //use the current state of hand to determine this turns score and update the scorecard accordingly
-        String[][] comboChoices = card.getPossibleCombosChoices( hand.getValues() );
-
-        //if a player doesn't have any possible choices they get 0 points, ie the 'makeChoice' method doesn't need to run
-        if (comboChoices.length == 0) {
-            return false;
-        }
-
-        //this needs a minus one because the index will be minus one from their selection
-        int comboIndex = YahtzeeCLI.chooseCombo(comboChoices) - 1;
-
-        int turnPoints = card.makeChoice( comboChoices[comboIndex][0] );
-
-        //add the points earned for this round
-        score += turnPoints;
-
-        return true;
-
-    }
-
-    private List<Integer> parseMultipleDiceInput(String usersChoices) {
-
-        List<Integer> indices =
-                Arrays.stream(
-                usersChoices
-                    .replaceAll("\\D+", "") //regex to replace all the non number chars
-                    .split(""))//split into the individual chars
-                    .filter(str -> !str.equals(""))
-                    .mapToInt(Integer::parseInt) //convert to numbers
-//                    .forEach( System.out::println);
-                    .filter(n -> n > 0 && n < 6) //filter out all number out of the possible range
-                    .limit(5) //only allow the first 5 numbers input that were within the range
-                    .map( n -> n-1) //set the values to their index equivalent
-                .boxed() //convert from regular list to List<Integer>
-                .collect(Collectors.toList()); //finally convert to a List
-
-        return indices;
-    }
-
-    private boolean isTurnOver(int currentRoll) {
+    protected boolean isTurnOver(int currentRoll) {
         //allow the user to end their turn prematurely
         boolean endTurn = false;
         //this check that it makes sense to ask them if they want to end their turn,
