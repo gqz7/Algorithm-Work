791f55f3a08166ee3daba06339bf3a4f773fcda4
GabrielQSherman <gabrielquinn.photography@gmail.com>
2020-11-20 23:50:20 -0500

    example uses of takeWhile, peek, mapToInt and reduce

--- Java/AdvJavaConcepts/Streams/Stream_Interm_Examples.java
+++ Java/AdvJavaConcepts/Streams/Stream_Interm_Examples.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -23,6 +24,17 @@ public class Stream_Interm_Examples {
         System.out.println("\n\nMore Methods to Explore\n");
         System.out.println("take/dropWhile, allows filtering based on a conditional to occur until a condition is met");
         System.out.println("peek: allow you to see/log data in a stream without terminating it, a terminating equivalent can be made with forEach");
+
+        OptionalInt sumOfNumsBelowFour = Arrays.stream(strNumbers)
+                .takeWhile(numStr -> Integer.parseInt(numStr) < 4)
+                .peek(n -> System.out.println("peeking: " + n))
+                .mapToInt(Integer::parseInt)
+                .reduce(Integer::sum);
+
+        if (sumOfNumsBelowFour.isPresent())
+            System.out.println("Sum of numbers in array below 4: " + sumOfNumsBelowFour.getAsInt());
+
+
         System.out.println("\nReducing Methods");
         System.out.println("\treduce()");
         System.out.println("\tcount()");
